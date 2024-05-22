7307c501ef3f72cf815cfd9faf8d9c79b3e9c6a0
Amazon Translate <noreply-mt-aws@weblate.org>
2024-05-22 13:18:08 +0000

    Translated using Weblate (Romanian)
    Currently translated at 100.0% (10 of 10 strings)
    
    Translation: WeVote4Eu/app
    Translate-URL: https://translate.commitglobal.org/projects/wevote4eu/app/ro/

--- lang/ro/app.php
+++ lang/ro/app.php
@@ -1,7 +1,4 @@
 <?php
-
-declare(strict_types=1);
-
 return [
     'action' => [
         'back' => 'Înapoi',
@@ -10,10 +7,13 @@ return [
     'banner' => 'O soluție Commit Global.',
     'select_nationality' => 'Care este naționalitatea ta?',
     'parliament_elections_title' => 'Alegeri parlamentare',
-
     'navigation' => [
         'home' => 'Acasă',
         'about' => 'Despre',
     ],
-
+    'hero' => [
+        'description' => 'Inteligența automată este un produs consecvent. Ultricies are o mulțime de cuvinte. Garanția a fost garantată pentru diferite tipuri de țesături. Malesuada are o varietate de vieți și de ultimă generație.',
+        'title' => 'Împuternicirea democrației UE prin',
+        'name' => 'Votăm pentru UE',
+    ],
 ];
