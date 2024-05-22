f625a5cb436ed7d9487e3bd0f301117249b97790
Amazon Translate <noreply-mt-aws@weblate.org>
2024-05-22 13:17:50 +0000

    Translated using Weblate (Slovenian)
    Currently translated at 100.0% (10 of 10 strings)
    
    Translation: WeVote4Eu/app
    Translate-URL: https://translate.commitglobal.org/projects/wevote4eu/app/sl/

--- lang/sl/app.php
+++ lang/sl/app.php
@@ -1,8 +1,19 @@
 <?php
-
-declare(strict_types=1);
-
 return [
     'select_nationality' => 'Kakšno je vaše državljanstvo?',
     'parliament_elections_title' => 'Parlamentarne volitve',
+    'navigation' => [
+        'about' => 'O',
+        'home' => 'Domov',
+    ],
+    'hero' => [
+        'description' => 'Zdravstvena bolečina je zelo pomembna. Vsekakor so množično opisani. Tellus ID je bil namenjen garanciji. Malesuada ima veliko življenjsko dobo in izjemno hitrerit.',
+        'title' => 'krepitev demokracije EU prek',
+        'name' => 'Glasujemo za EU',
+    ],
+    'action' => [
+        'home' => 'Domov',
+        'back' => 'Nazaj',
+    ],
+    'banner' => 'Rešitev Commit Global.',
 ];
