7c1ae1eef8059b74b5f4229ae691ae754b4d49c4
Andrei Ioniță <andrei.ionita@commitglobal.org>
2024-05-22 13:44:25 +0000

    Translated using Weblate (Estonian)
    Currently translated at 90.0% (9 of 10 strings)
    
    Translation: WeVote4Eu/app
    Translate-URL: https://translate.commitglobal.org/projects/wevote4eu/app/et/

--- lang/et/app.php
+++ lang/et/app.php
@@ -7,7 +7,7 @@ return [
         'home' => 'Kodu',
     ],
     'hero' => [
-        'description' => 'Teenimiskõlblik on keskendunud. Väljakutsed on massiliselt ette nähtud. Tellus identifitseeritud kaubamärk oli garantii. Malesuada on palju elutähedaseid sõitmisi ja hämmastavaid elusid.',
+        'description' => 'Tutvustame platvormi, mille eesmärk on muuta 27 riiklikku Euroopa Parlamendi valimist ühtseks üleeuroopaliseks ürituseks. Me hääletame ELi poolt on terviklik digitaalne keskus, mis tsentraliseerib olulist valimisteavet ja ajakohastamist reaalajas, hõlbustades kodanike osalemist demokraatlikus protsessis.',
         'title' => 'ELi demokraatia võimendamine',
         'name' => 'Hääletame Euroopa Liidu poolt',
     ],
