0abf7b88d8ab283f93df7caaba32b263ef6541ee
Amazon Translate <noreply-mt-aws@weblate.org>
2024-05-22 13:18:05 +0000

    Translated using Weblate (Polish)
    Currently translated at 100.0% (10 of 10 strings)
    
    Translation: WeVote4Eu/app
    Translate-URL: https://translate.commitglobal.org/projects/wevote4eu/app/pl/

--- lang/pl/app.php
+++ lang/pl/app.php
@@ -1,8 +1,19 @@
 <?php
-
-declare(strict_types=1);
-
 return [
     'select_nationality' => 'Jaka jest twoja narodowość?',
     'parliament_elections_title' => 'Wybory parlamentarne',
+    'navigation' => [
+        'about' => 'O',
+        'home' => 'Strona główna',
+    ],
+    'hero' => [
+        'description' => 'Prawdziwe uczucie bólu jest bardzo ważne. Określenie jest masowo opisane. Tellus ID kasyna był objęty gwarancją. Malesuada ma różne formy życia w niszlu i bardzo rzadko.',
+        'title' => 'Wzmocnienie demokracji w UE poprzez',
+        'name' => 'Głosujemy na UE',
+    ],
+    'action' => [
+        'home' => 'Strona główna',
+        'back' => 'Wstecz',
+    ],
+    'banner' => 'Rozwiązanie Commit Global.',
 ];
