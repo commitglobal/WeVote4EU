da0d471023c522ac0241ede7d71b75ab6d7c939d
Amazon Translate <noreply-mt-aws@weblate.org>
2024-05-22 13:17:51 +0000

    Translated using Weblate (Swedish)
    Currently translated at 100.0% (10 of 10 strings)
    
    Translation: WeVote4Eu/app
    Translate-URL: https://translate.commitglobal.org/projects/wevote4eu/app/sv/

--- lang/sv/app.php
+++ lang/sv/app.php
@@ -1,8 +1,19 @@
 <?php
-
-declare(strict_types=1);
-
 return [
     'select_nationality' => 'Uyruğunuz nedir?',
     'parliament_elections_title' => 'Parlamento seçimleri',
+    'navigation' => [
+        'about' => 'Om',
+        'home' => 'Hem',
+    ],
+    'hero' => [
+        'description' => 'Det är en självklarhet som leder till en personlig upplevelse. Ultricies är en massa ord för att göra det. Det är garanterat att det finns en garanti för olika typer av produkter. Malesuada har en mängd olika liv i och extremt höga halter.',
+        'title' => 'Stärka EU:s demokrati genom',
+        'name' => 'Vi röstar på EU',
+    ],
+    'action' => [
+        'home' => 'Hem',
+        'back' => 'Tillbaka',
+    ],
+    'banner' => 'En Commit Global lösning.',
 ];
