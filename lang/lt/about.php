ec544315a730b51747ed691f2257a7f67eeadc77
Amazon Translate <noreply-mt-aws@weblate.org>
2024-05-22 14:53:24 +0000

    Translated using Weblate (Lithuanian)
    Currently translated at 100.0% (10 of 10 strings)
    
    Translation: WeVote4Eu/about
    Translate-URL: https://translate.commitglobal.org/projects/wevote4eu/about/lt/

--- lang/lt/about.php
+++ lang/lt/about.php
@@ -1,13 +1,13 @@
 <?php
 return [
-    'title' => '',
-    'line_1' => '',
-    'line_2' => '',
-    'subtitle' => '',
-    'line_3' => '',
-    'line_4' => '',
-    'line_5' => '',
-    'line_6' => '',
-    'line_7' => '',
-    'line_8' => '',
+    'title' => 'Apie projektą',
+    'line_1' => 'Įsivaizduokite vieną, gyvybingą rinkimų renginį, vienijantį per 400 milijonų piliečių visoje Europos Sąjungoje. ES rinkimai yra ne tik nacionalinių balsų serija, bet ir kolektyvinis mūsų bendros tapatybės ir demokratinių vertybių šventė. Būdami ES piliečiais susivienijame, kad formuotume savo bendrą ateitį, peržengdami sienas ir atsižvelgdami į mūsų įvairovės stiprybę. Šis projektas yra mūsų vienybės liudijimas, leidžiantis kiekvienam piliečiui, nesvarbu, kur jis yra ES, lengviau dalyvauti šiame galingame demokratiniame procese.',
+    'line_2' => 'Siekdama paremti šią vieningo rinkimų proceso viziją, “Commit Global” sukūrė platformą**We Vote for EU**, kuri įkūnija vienybės ir bendradarbiavimo idėją. Ši platforma suteikia visiems ES piliečiams svarbią informaciją apie tai, kaip greitai balsuoti ir rasti rinkimų apylinkes, nepriklausomai nuo jų buvimo vietos.',
+    'subtitle' => 'Kuo ši platforma yra ypatinga?',
+    'line_3' => '**Sprendimų medžio formatas: ** Platformoje naudojamas sprendimų medžio formatas, kuriuo ES piliečiai tiesiogiai nukreipiami prie konkrečios jiems reikalingos informacijos.',
+    'line_4' => '**Išsami informacija: ** Galutinė pateikta informacija yra išsami, apimanti registracijos reikalavimus (jei yra), skirtingus balsavimo būdus (įgaliotinis, internetu, paštas, rinkimų apylinkė), reikalingą balsavimo dokumentaciją ir pagalbą rinkėjams su negalia ar kitais apribojimais.',
+    'line_5' => '**Patikrinta informacija: ** Svetainėje pateikiama informacija kryžmiškai tikrinama iš kelių skirtingų šaltinių, įskaitant nacionalines ministerijas ir organizacijas, atsakingas už balsavimo taisykles ir procedūras, taip pat iš Europos Parlamento platformos.',
+    'line_6' => '**Prieinamumas daugiakalbiams: ** Užtikriname, kad kiekvienas ES pilietis galėtų susipažinti su reikiama informacija savo kalba. Platforma prieinama 26 kalbomis, o kiekvienos šalies sprendimų medis pateikiamas jos oficialiąja (-omis) kalba (-omis) ir anglų kalba. Tai užtikrina, kad kiekvienas, nesvarbu, koks yra jų kalbinis pagrindas, galėtų be vargo naršyti balsavimo procese.',
+    'line_7' => '**Tarpusavio gairės: ** Nepriklausomai nuo rinkėjo ES pilietybės, jo gyvenamosios šalies ar buvimo vietos rinkimų dieną, platformoje pateikiamos tikslios, pritaikytos instrukcijos ir informacija, susijusi su jų unikalia padėtimi. Tai tarsi turėti asmeninį balsavimo asistentą, kuris daro procesą sklandų ir įkvepiantį.',
+    'line_8' => '“We Vote for EU” yra platforma, kurią sukūrė ir valdo pirmoji pasaulyje pilietinių technologijų nevyriausybinė organizacija **Commit Global**. “Commit Global”, kurios būstinė yra Hagoje (Nyderlandai), konsoliduoja, palaiko ir suteikia laisvą prieigą prie ypatingos svarbos skaitmeninės, “Infrastructure for Good”, pilietinės visuomenės organizacijoms ir nukentėjusiems gyventojams visame pasaulyje. Norėdami gauti daugiau informacijos apie mūsų darbą, apsilankykite [mūsų svetainėje] (https://www.commitglobal.org).',
 ];
