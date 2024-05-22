adfe10108324cafb6ed5192eef737707c288a698
Amazon Translate <noreply-mt-aws@weblate.org>
2024-05-22 13:18:01 +0000

    Translated using Weblate (Lithuanian)
    Currently translated at 100.0% (10 of 10 strings)
    
    Translation: WeVote4Eu/app
    Translate-URL: https://translate.commitglobal.org/projects/wevote4eu/app/lt/

--- lang/lt/app.php
+++ lang/lt/app.php
@@ -1,8 +1,19 @@
 <?php
-
-declare(strict_types=1);
-
 return [
     'select_nationality' => 'Kokia jūsų tautybė?',
     'parliament_elections_title' => 'Parlamento rinkimai',
+    'navigation' => [
+        'about' => 'Apie',
+        'home' => 'Namai',
+    ],
+    'hero' => [
+        'description' => 'Skausmas yra labai svarbus dalykas. Apie tai, kad jie yra labai svarbūs. “Tellus ID” kazino dėžė buvo garantuota. Malesuada įvairių gyvybių ir labai mažų dienų.',
+        'title' => 'ES demokratijos įgalinimas per',
+        'name' => 'Mes balsuojame už ES',
+    ],
+    'action' => [
+        'home' => 'Namai',
+        'back' => 'Atgal',
+    ],
+    'banner' => '“Commit Global” sprendimas.',
 ];
