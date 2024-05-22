d451c3c2a044ec2ae72dc4f6879f3d03669bb166
Andrei Ioniță <hi@andrei.io>
2024-05-22 17:42:06 +0300

    about page

---
+++ lang/en/about.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+
+    'title' => 'About the project',
+
+    'line_1' => 'Imagine a single, vibrant election event that unites over 400 million citizens across the European Union. EU elections are not just a series of national votes but a collective celebration of our shared identity and democratic values. As EU citizens, we come together to shape our common future, transcending borders and embracing the strength of our diversity. This project is a testament to our unity, making it easier for every citizen, no matter where they are in the EU, to participate in this powerful democratic process.',
+
+    'line_2' => 'To support this vision of a unified election process, Commit Global has created a platform, **We Vote for EU**, which embodies the idea of unity and collaboration. This platform provides all EU citizens with essential information on how to cast their votes and find polling stations quickly, regardless of their location.',
+
+    'subtitle' => 'What makes this platform special?',
+
+    'line_3' => '**Decision Tree Format:** The platform uses a decision tree format that guides EU citizens directly to the specific information they need.',
+
+    'line_4' => '**Comprehensive Information:** The final information provided is comprehensive, covering registration requirements (if any), different voting methods (proxy, online, postal, polling station), required documentation for voting, and assistance for voters with disabilities or other limitations.',
+
+    'line_5' => '**Verified Information:** The information presented on the website is cross checked from multiple different sources, including national ministries and organizations responsible for voting rules and procedures, as well as the European Parliament platform.',
+
+    'line_6' => '**Multilingual Accessibility:** We make sure that every EU citizen can access the information they need in their own language. The platform is available in 26 languages, with each country\'s decision tree provided in its official language(s) and in English. This ensures that everyone, no matter their linguistic background, can navigate the voting process effortlessly.',
+
+    'line_7' => '**Interconnected Guidance:** Regardless of the EU nationality of the voter, their country of residence or their location on the day of the elections, the platform provides precise, tailored instructions and information relevant to their unique situation. It’s like having a personalized voting assistant that makes the process smooth and inspiring.',
+
+    'line_8' => 'We Vote for EU is a platform created and managed by **Commit Global**, the world\'s first global civic tech NGO. With the headquarter in the Hague (The Netherlands), Commit Global consolidates, maintains and provides free access to critical digital, the **Infrastructure for Good**, to civil society organizations and affected populations around the world. For further information on our work, please visit [our website](https://www.commitglobal.org).',
+];
--- resources/views/livewire/pages/about.blade.php
+++ resources/views/livewire/pages/about.blade.php
@@ -1,3 +1,18 @@
-<div>
-    {{-- Be like water. --}}
+<div class="px-6 py-32 lg:px-8">
+    <div
+        class="max-w-3xl mx-auto prose text-gray-700 md:prose-lg lg:prose-xl prose-a:text-primary-500 prose-a:font-medium hover:prose-a:no-underline prose-headings:text-primary-500">
+        <h1>{!! Str::inlineMarkdown(__('about.title')) !!}</h1>
+
+        {!! Str::markdown(__('about.line_1')) !!}
+        {!! Str::markdown(__('about.line_2')) !!}
+
+        <h2>{!! Str::inlineMarkdown(__('about.subtitle')) !!}</h2>
+
+        {!! Str::markdown(__('about.line_3')) !!}
+        {!! Str::markdown(__('about.line_4')) !!}
+        {!! Str::markdown(__('about.line_5')) !!}
+        {!! Str::markdown(__('about.line_6')) !!}
+        {!! Str::markdown(__('about.line_7')) !!}
+        {!! Str::markdown(__('about.line_8')) !!}
+    </div>
 </div>
