13480cf7ce8448ba3193d4f6b4d5f5f7ee7bb8ce
Amazon Translate <noreply-mt-aws@weblate.org>
2024-05-22 14:53:45 +0000

    Translated using Weblate (Portuguese)
    Currently translated at 100.0% (10 of 10 strings)
    
    Translation: WeVote4Eu/about
    Translate-URL: https://translate.commitglobal.org/projects/wevote4eu/about/pt/

--- lang/pt/about.php
+++ lang/pt/about.php
@@ -1,13 +1,13 @@
 <?php
 return [
-    'title' => '',
-    'line_1' => '',
-    'line_2' => '',
-    'subtitle' => '',
-    'line_3' => '',
-    'line_4' => '',
-    'line_5' => '',
-    'line_6' => '',
-    'line_7' => '',
-    'line_8' => '',
+    'title' => 'Sobre o projeto',
+    'line_1' => 'Imagine um evento eleitoral único e vibrante que une mais de 400 milhões de cidadãos em toda a União Europeia. As eleições da UE não são apenas uma série de votações nacionais, mas uma celebração coletiva da nossa identidade comum e dos valores democráticos. Como cidadãos da UE, nos unimos para moldar nosso futuro comum, transcendendo fronteiras e abraçando a força de nossa diversidade. Este projeto é uma prova da nossa unidade, tornando mais fácil para todos os cidadãos, não importa onde estejam na UE, participar desse poderoso processo democrático.',
+    'line_2' => 'Para apoiar essa visão de um processo eleitoral unificado, a Commit Global criou uma plataforma, **We Vote for EU**, que incorpora a ideia de unidade e colaboração. Esta plataforma fornece a todos os cidadãos da UE informações essenciais sobre como votar e encontrar seções eleitorais rapidamente, independentemente de sua localização.',
+    'subtitle' => 'O que torna essa plataforma especial?',
+    'line_3' => '**Formato de árvore de decisão: ** A plataforma usa um formato de árvore de decisão que orienta os cidadãos da UE diretamente às informações específicas de que precisam.',
+    'line_4' => '**Informações abrangentes: ** As informações finais fornecidas são abrangentes, abrangendo requisitos de registro (se houver), diferentes métodos de votação (por procuração, on-line, postal, seção eleitoral), documentação necessária para votação e assistência para eleitores com deficiências ou outras limitações.',
+    'line_5' => '**Informações verificadas: ** As informações apresentadas no site são verificadas de várias fontes diferentes, incluindo ministérios e organizações nacionais responsáveis pelas regras e procedimentos de votação, bem como a plataforma do Parlamento Europeu.',
+    'line_6' => '**Acessibilidade multilíngue: ** Garantimos que todos os cidadãos da UE possam acessar as informações de que precisam em seu próprio idioma. A plataforma está disponível em 26 idiomas, com a árvore decisória de cada país fornecida em seu idioma oficial e em inglês. Isso garante que todos, independentemente de sua formação linguística, possam navegar pelo processo de votação sem esforço.',
+    'line_7' => '**Orientação interconectada: ** Independentemente da nacionalidade europeia do eleitor, do país de residência ou da localização no dia das eleições, a plataforma fornece instruções precisas e personalizadas e informações relevantes para sua situação específica. É como ter um assistente de votação personalizado que torna o processo suave e inspirador.',
+    'line_8' => 'We Vote for EU é uma plataforma criada e gerenciada pela **Commit Global**, a primeira ONG global de tecnologia cívica do mundo. Com sede em Haia (Holanda), a Commit Global consolida, mantém e fornece acesso gratuito à mídia digital crítica, a **Infrastructure for Good**, para organizações da sociedade civil e populações afetadas em todo o mundo. Para obter mais informações sobre nosso trabalho, visite [nosso site] (https://www.commitglobal.org).',
 ];
