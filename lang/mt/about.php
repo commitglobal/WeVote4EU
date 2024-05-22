39f328da4705087fb8c9956a7dfb06ba51d3fe65
Amazon Translate <noreply-mt-aws@weblate.org>
2024-05-22 14:53:29 +0000

    Translated using Weblate (Maltese)
    Currently translated at 100.0% (10 of 10 strings)
    
    Translation: WeVote4Eu/about
    Translate-URL: https://translate.commitglobal.org/projects/wevote4eu/about/mt/

--- lang/mt/about.php
+++ lang/mt/about.php
@@ -1,13 +1,13 @@
 <?php
 return [
-    'title' => '',
-    'line_1' => '',
-    'line_2' => '',
-    'subtitle' => '',
-    'line_3' => '',
-    'line_4' => '',
-    'line_5' => '',
-    'line_6' => '',
-    'line_7' => '',
-    'line_8' => '',
+    'title' => 'Dwar il-proġett',
+    'line_1' => 'Immaġina avveniment elettorali wieħed u vibranti li jgħaqqad aktar minn 400 miljun ċittadin madwar l-Unjoni Ewropea. L-elezzjonijiet tal-UE mhumiex biss sensiela ta\' voti nazzjonali iżda ċelebrazzjoni kollettiva tal-identità kondiviża u tal-valuri demokratiċi tagħna. Bħala ċittadini tal-UE, aħna niltaqgħu flimkien biex insawru l-futur komuni tagħna, billi ntraxxendu l-fruntieri u nħaddnu s-saħħa tad-diversità tagħna. Dan il-proġett huwa xhieda tal-għaqda tagħna, u jagħmilha aktar faċli għal kull ċittadin, irrispettivament minn fejn ikun fl-UE, biex jipparteċipa f\'dan il-proċess demokratiku qawwi.',
+    'line_2' => 'Biex tappoġġja din il-viżjoni ta \'proċess ta\' elezzjoni unifikat, Commit Global ħoloq pjattaforma, **We Vote for EU**, li tinkorpora l-idea ta \'għaqda u kollaborazzjoni. Din il-pjattaforma tipprovdi liċ-ċittadini kollha tal-UE b\'informazzjoni essenzjali dwar kif jitfgħu l-voti tagħhom u jsibu postijiet tal-votazzjoni malajr, irrispettivament mill-post tagħhom.',
+    'subtitle' => 'X\'jagħmel din il-pjattaforma speċjali?',
+    'line_3' => '**Format tas-Siġra tad-Deċiżjoni: ** Il-pjattaforma tuża format tas-siġra tad-deċiżjonijiet li jiggwida liċ-ċittadini tal-UE direttament għall-informazzjoni speċifika li għandhom bżonn.',
+    'line_4' => '**Informazzjoni Komprensiva: ** L-informazzjoni finali pprovduta hija komprensiva, li tkopri r-rekwiżiti tar-reġistrazzjoni (jekk ikun hemm), metodi differenti tal-votazzjoni (prokura, onlajn, postali, stazzjon tal-votazzjoni), dokumentazzjoni meħtieġa għall-votazzjoni, u assistenza għall-votanti b\'diżabilità jew limitazzjonijiet oħra.',
+    'line_5' => '**Informazzjoni Verifikata: ** L-informazzjoni ppreżentata fuq il-websajt hija kontroverifikata minn sorsi differenti multipli, inklużi ministeri nazzjonali u organizzazzjonijiet responsabbli għar-regoli u l-proċeduri tal-votazzjoni, kif ukoll il-pjattaforma tal-Parlament Ewropew.',
+    'line_6' => '**Aċċessibbiltà Multilingwi: ** Aħna niżguraw li kull ċittadin tal-UE jkun jista\' jaċċessa l-informazzjoni li jeħtieġ fil-lingwa tiegħu stess. Il-pjattaforma hija disponibbli f\'26 lingwa, bis-siġra tad-deċiżjonijiet ta\' kull pajjiż ipprovduta fil-lingwa/i uffiċjali tiegħu u bl-Ingliż. Dan jiżgura li kulħadd, irrispettivament mill-isfond lingwistiku tiegħu, ikun jista\' jinnaviga l-proċess tal-votazzjoni bla sforz.',
+    'line_7' => '**Gwida Interkonnessa: ** Irrispettivament min-nazzjonalità tal-UE tal-votant, il-pajjiż ta\' residenza tiegħu jew il-post tiegħu fil-jum tal-elezzjonijiet, il-pjattaforma tipprovdi struzzjonijiet preċiżi u mfassla mfassla u informazzjoni rilevanti għas-sitwazzjoni unika tagħhom. Huwa simili li jkollok assistent tal-votazzjoni personalizzat li jagħmel il-proċess bla xkiel u jispira.',
+    'line_8' => 'We Vote for EU hija pjattaforma maħluqa u ġestita mi**Commit Global**, l-ewwel NGO globali tat-teknoloġija ċivika fid-dinja. Bil-kwartieri ġenerali fl-Aja (L-Olanda), Commit Global jikkonsolida, iżomm u jipprovdi aċċess liberu għad-diġitali kritika, l-**Infrastruttura għall-Tajba**, lill-organizzazzjonijiet tas-soċjetà ċivili u l-popolazzjonijiet affettwati madwar id-dinja. Għal aktar informazzjoni dwar ix-xogħol tagħna, jekk jogħġbok żur [websajt tagħna] (https://www.commitglobal.org).',
 ];
