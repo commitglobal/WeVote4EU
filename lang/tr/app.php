5d45641b356aba29eadef907335fffbcbfafabe5
Amazon Translate <noreply-mt-aws@weblate.org>
2024-05-22 13:17:52 +0000

    Translated using Weblate (Turkish)
    Currently translated at 100.0% (10 of 10 strings)
    
    Translation: WeVote4Eu/app
    Translate-URL: https://translate.commitglobal.org/projects/wevote4eu/app/tr/

--- lang/tr/app.php
+++ lang/tr/app.php
@@ -1,8 +1,19 @@
 <?php
-
-declare(strict_types=1);
-
 return [
     'select_nationality' => 'Mi az állampolgársága?',
     'parliament_elections_title' => 'Parlamenti választások',
+    'navigation' => [
+        'about' => 'Hakkında',
+        'home' => 'Ana Sayfa',
+    ],
+    'hero' => [
+        'description' => 'Özellikten çok hassas bir ortam sağlanır. Ultricies kitlesel olarak adlandırılır. Teslimatın kimliği belirli bir şekilde garanti altına alınmıştır. Malesuada, çok çeşitli yaşam süreleri ve son derece hızlı bir şekilde taşınır.',
+        'title' => 'AB Demokrasisini Güçlendirmek',
+        'name' => 'AB\'ye oy veriyoruz',
+    ],
+    'action' => [
+        'home' => 'Ana Sayfa',
+        'back' => 'Geri',
+    ],
+    'banner' => 'Bir Commit Global çözümü.',
 ];
