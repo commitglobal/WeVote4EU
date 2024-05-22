1f92e9748d8809b717b04992a46a80837f76bee3
Amazon Translate <noreply-mt-aws@weblate.org>
2024-05-22 14:52:50 +0000

    Translated using Weblate (Finnish)
    Currently translated at 100.0% (10 of 10 strings)
    
    Translation: WeVote4Eu/about
    Translate-URL: https://translate.commitglobal.org/projects/wevote4eu/about/fi/

--- lang/fi/about.php
+++ lang/fi/about.php
@@ -1,13 +1,13 @@
 <?php
 return [
-    'title' => '',
-    'line_1' => '',
-    'line_2' => '',
-    'subtitle' => '',
-    'line_3' => '',
-    'line_4' => '',
-    'line_5' => '',
-    'line_6' => '',
-    'line_7' => '',
-    'line_8' => '',
+    'title' => 'Tietoa projektista',
+    'line_1' => 'Kuvittele yksi, vilkas vaalitapahtuma, joka yhdistää yli 400 miljoonaa kansalaista eri puolilla Euroopan unionia. EU-vaalit eivät ole vain kansallisten äänestysten sarja, vaan yhteisen identiteettimme ja demokraattisten arvojemme yhteinen juhla. EU:n kansalaisina me yhdessä muokkaamme yhteistä tulevaisuuttamme ylittämällä rajat ja omaksumalla monimuotoisuutemme vahvuuden. Tämä hanke on osoitus yhtenäisyydestämme ja helpottaa jokaisen kansalaisen osallistumista tähän voimakkaaseen demokraattiseen prosessiin riippumatta siitä, missä he ovat EU:ssa.',
+    'line_2' => 'Tukeakseen tätä näkemystä yhtenäisestä vaaliprosessista Commit Global on luonut foorumin, **We Vote for EU**, joka ilmentää ajatusta yhtenäisyydestä ja yhteistyöstä. Tämä foorumi tarjoaa kaikille EU:n kansalaisille olennaista tietoa siitä, miten he voivat antaa äänensä ja löytää äänestyspaikat nopeasti sijainnista riippumatta.',
+    'subtitle' => 'Mikä tekee tästä alustasta erityisen?',
+    'line_3' => '**Päätöspuumuoto: ** Alusta käyttää päätöspuumuotoa, joka ohjaa EU:n kansalaiset suoraan tarvitsemiinsa erityistietoihin.',
+    'line_4' => '**Kattavat tiedot: ** Lopulliset tiedot ovat kattavia, ja ne kattavat rekisteröintivaatimukset (mahdolliset), erilaiset äänestysmenetelmät (valtakirja, verkko, posti, äänestyspaikka), vaaditut asiakirjat äänestykseen ja avun vammaisille äänestäjille tai muut rajoitukset.',
+    'line_5' => '**Vahvistetut tiedot: ** Verkkosivustolla esitetyt tiedot tarkistetaan useista eri lähteistä, mukaan lukien kansallisista ministeriöistä ja äänestyssäännöistä ja -menettelyistä vastaavista järjestöistä sekä Euroopan parlamentin foorumista.',
+    'line_6' => '**Monikielinen saavutettavuus: ** Varmistamme, että jokainen EU-kansalainen voi saada tarvitsemansa tiedot omalla kielellään. Alusta on saatavilla 26 kielellä, ja kunkin maan päätöksentekopuu toimitetaan sen virallisilla kielillä ja englanniksi. Näin varmistetaan, että jokainen, kielellisestä taustastaan riippumatta, voi navigoida äänestysprosessissa vaivattomasti.',
+    'line_7' => '**Yhteenkytketyt ohjeet: ** Riippumatta äänestäjän EU-kansalaisuudesta, asuinmaasta tai sijainnista vaalipäivänä foorumi tarjoaa täsmällisiä, räätälöityjä ohjeita ja tietoja, jotka liittyvät hänen yksilölliseen tilanteeseensa. Se on kuin henkilökohtainen äänestysavustaja, joka tekee prosessista sujuvan ja inspiroivan.',
+    'line_8' => 'We Vote for EU on foorumi, jonka on luonut ja hallinnoinut **Commit Global**, maailman ensimmäinen maailmanlaajuinen kansalaisteknologian kansalaisjärjestö. Commit Global, jonka pääkonttori sijaitsee Haagissa (Alankomaat), yhdistää, ylläpitää ja tarjoaa ilmaisen pääsyn kriittiseen digitaaliseen, **Infrastructure for Good**, kansalaisyhteiskunnan organisaatioille ja kärsineille väestöille ympäri maailmaa. Lisätietoja työstämme on osoitteessa [verkkosivustollamme] (https://www.commitglobal.org).',
 ];
