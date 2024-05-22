5e5445b8452c2144947a29dd1b6acde45692cf0c
Amazon Translate <noreply-mt-aws@weblate.org>
2024-05-22 13:18:08 +0000

    Translated using Weblate (Slovak)
    Currently translated at 100.0% (10 of 10 strings)
    
    Translation: WeVote4Eu/app
    Translate-URL: https://translate.commitglobal.org/projects/wevote4eu/app/sk/

--- lang/sk/app.php
+++ lang/sk/app.php
@@ -1,8 +1,19 @@
 <?php
-
-declare(strict_types=1);
-
 return [
     'select_nationality' => 'Aká je vaša národnosť?',
     'parliament_elections_title' => 'Voľby do parlamentu',
+    'navigation' => [
+        'about' => 'O',
+        'home' => 'Domov',
+    ],
+    'hero' => [
+        'description' => 'Ľudská bolesť je veľmi dôležitá. Vôbec sa hovorí, že je to veľmi dôležité. Tellus ID kasína bola zaručená. Malesúada je veľmi dobrá životnosť a nesmierny život.',
+        'title' => 'Posilnenie demokracie EÚ prostredníctvom',
+        'name' => 'Hlasujeme za EÚ',
+    ],
+    'action' => [
+        'home' => 'Domov',
+        'back' => 'Späť',
+    ],
+    'banner' => 'Commit Global riešenie.',
 ];
