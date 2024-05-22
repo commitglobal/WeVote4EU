9d25e56829cafd950ac27834514744e6666e60eb
Amazon Translate <noreply-mt-aws@weblate.org>
2024-05-22 14:53:26 +0000

    Translated using Weblate (Latvian)
    Currently translated at 100.0% (10 of 10 strings)
    
    Translation: WeVote4Eu/about
    Translate-URL: https://translate.commitglobal.org/projects/wevote4eu/about/lv/

--- lang/lv/about.php
+++ lang/lv/about.php
@@ -1,13 +1,13 @@
 <?php
 return [
-    'title' => '',
-    'line_1' => '',
-    'line_2' => '',
-    'subtitle' => '',
-    'line_3' => '',
-    'line_4' => '',
-    'line_5' => '',
-    'line_6' => '',
-    'line_7' => '',
-    'line_8' => '',
+    'title' => 'Par projektu',
+    'line_1' => 'Iedomājieties vienu, dinamisku vēlēšanu pasākumu, kas apvieno vairāk nekā 400 miljonus pilsoņu visā Eiropas Savienībā. ES vēlēšanas nav tikai valstu balsojumu sērija, bet arī mūsu kopīgās identitātes un demokrātisko vērtību kopīga svinēšana. Kā ES pilsoņi mēs apvienojamies, lai veidotu savu kopējo nākotni, pārsniedzot robežas un aptverot mūsu daudzveidības spēku. Šis projekts liecina par mūsu vienotību, atvieglojot ikvienam pilsonim neatkarīgi no tā, kur viņi atrodas ES, piedalīties šajā spēcīgajā demokrātiskajā procesā.',
+    'line_2' => 'Lai atbalstītu šo redzējumu par vienotu vēlēšanu procesu, Commit Global ir izveidojis platformu “We Vote for EU**”, kas iemieso vienotības un sadarbības ideju. Šī platforma sniedz visiem ES pilsoņiem būtisku informāciju par to, kā ātri nodot balsojumus un atrast vēlēšanu iecirkņus neatkarīgi no viņu atrašanās vietas.',
+    'subtitle' => 'Kas padara šo platformu īpašu?',
+    'line_3' => '**Lēmumu koka formāts: ** Platforma izmanto lēmumu koka formātu, kas ES pilsoņiem sniedz tieši vajadzīgo informāciju.',
+    'line_4' => '**Visaptveroša informācija: ** Galīgā informācija ir visaptveroša, aptverot reģistrācijas prasības (ja tādas ir), dažādas balsošanas metodes (pilnvarnieks, tiešsaistē, pasts, vēlēšanu iecirknis), balsošanai nepieciešamo dokumentāciju un palīdzību vēlētājiem ar invaliditāti vai citiem ierobežojumiem.',
+    'line_5' => '**Pārbaudīta informācija: ** Tīmekļa vietnē sniegtā informācija tiek savstarpēji pārbaudīta no vairākiem dažādiem avotiem, tostarp valstu ministrijām un organizācijām, kas atbild par balsošanas noteikumiem un procedūrām, kā arī no Eiropas Parlamenta platformas.',
+    'line_6' => '**Pieejamība daudzvalodu valodā: ** Mēs nodrošinām, lai katrs ES pilsonis varētu piekļūt vajadzīgajai informācijai savā valodā. Platforma ir pieejama 26 valodās, un katras valsts lēmumu koks ir norādīts tās oficiālajā (-ās) valodā (-ās) un angļu valodā. Tas nodrošina, ka ikviens, neatkarīgi no viņu valodas pieredzes, var bez piepūles orientēties balsošanas procesā.',
+    'line_7' => '**Savstarpēji saistīti norādījumi: ** Neatkarīgi no vēlētāja ES pilsonības, viņa dzīvesvietas valsts vai atrašanās vietas vēlēšanu dienā platforma sniedz precīzus, pielāgotus norādījumus un informāciju, kas attiecas uz viņu unikālo situāciju. Tas ir tāpat kā personalizēts balsošanas palīgs, kas padara procesu vienmērīgu un iedvesmojošu.',
+    'line_8' => 'We Vote for EU ir platforma, kuru izveidojusi un pārvalda pasaulē pirmā globālā pilsonisko tehnoloģiju NVO **Commit Global**. Ar galveno mītni Hāgā (Nīderlande) Commit Global apvieno, uztur un nodrošina brīvu piekļuvi kritiskajai digitālajai struktūrai, **Infrastructure for Good** pilsoniskās sabiedrības organizācijām un skartajiem iedzīvotājiem visā pasaulē. Lai iegūtu papildinformāciju par mūsu darbu, lūdzu, apmeklējiet [mūsu tīmekļa vietni] (https://www.commitglobal.org).',
 ];
