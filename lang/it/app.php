b65503f18fb05c02bc53f3b03359f77541f9a485
Amazon Translate <noreply-mt-aws@weblate.org>
2024-05-22 13:18:01 +0000

    Translated using Weblate (Italian)
    Currently translated at 100.0% (10 of 10 strings)
    
    Translation: WeVote4Eu/app
    Translate-URL: https://translate.commitglobal.org/projects/wevote4eu/app/it/

--- lang/it/app.php
+++ lang/it/app.php
@@ -1,8 +1,19 @@
 <?php
-
-declare(strict_types=1);
-
 return [
     'select_nationality' => 'Qual è la sua nazionalità?',
     'parliament_elections_title' => 'Elezioni parlamentari',
+    'navigation' => [
+        'about' => 'Informazioni su',
+        'home' => 'Home',
+    ],
+    'hero' => [
+        'description' => 'Lorem ipsum dolor sit amet consectetur. Ultricies sit massa dictum senectus ut. Tellus di varius tellus consectetur era sospeso. Malesuada varius vitae nisl e ipsum ultricies hendrerit nisl.',
+        'title' => 'Potenziare la democrazia dell\'UE attraverso',
+        'name' => 'Votiamo per l\'UE',
+    ],
+    'action' => [
+        'home' => 'Home',
+        'back' => 'Indietro',
+    ],
+    'banner' => 'Una soluzione Commit Global.',
 ];
