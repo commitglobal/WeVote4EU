b720a6172f15ba894a995d6fde010f3a3875695e
Amazon Translate <noreply-mt-aws@weblate.org>
2024-05-22 13:17:55 +0000

    Translated using Weblate (Estonian)
    Currently translated at 100.0% (10 of 10 strings)
    
    Translation: WeVote4Eu/app
    Translate-URL: https://translate.commitglobal.org/projects/wevote4eu/app/et/

--- lang/et/app.php
+++ lang/et/app.php
@@ -1,8 +1,19 @@
 <?php
-
-declare(strict_types=1);
-
 return [
     'select_nationality' => 'Mis on teie kodakondsus?',
     'parliament_elections_title' => 'Parlamendi valimised',
+    'navigation' => [
+        'about' => 'Umbes',
+        'home' => 'Kodu',
+    ],
+    'hero' => [
+        'description' => 'Teenimiskõlblik on keskendunud. Väljakutsed on massiliselt ette nähtud. Tellus identifitseeritud kaubamärk oli garantii. Malesuada on palju elutähedaseid sõitmisi ja hämmastavaid elusid.',
+        'title' => 'ELi demokraatia võimendamine',
+        'name' => 'Hääletame Euroopa Liidu poolt',
+    ],
+    'action' => [
+        'home' => 'Kodu',
+        'back' => 'Tagasi',
+    ],
+    'banner' => 'Commit Global lahendus.',
 ];
