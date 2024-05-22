a3f915fa9e325ed6f010a287a04d49e9cd160d0c
Amazon Translate <noreply-mt-aws@weblate.org>
2024-05-22 14:51:57 +0000

    Translated using Weblate (Romanian)
    Currently translated at 100.0% (10 of 10 strings)
    
    Translation: WeVote4Eu/about
    Translate-URL: https://translate.commitglobal.org/projects/wevote4eu/about/ro/

--- lang/ro/about.php
+++ lang/ro/about.php
@@ -1,13 +1,13 @@
 <?php
 return [
-    'title' => '',
-    'line_1' => '',
-    'line_2' => '',
-    'subtitle' => '',
-    'line_3' => '',
-    'line_4' => '',
-    'line_5' => '',
-    'line_6' => '',
-    'line_7' => '',
-    'line_8' => '',
+    'title' => 'Despre proiect',
+    'line_1' => 'Imaginați-vă un eveniment electoral unic, vibrant, care unește peste 400 de milioane de cetățeni din întreaga Uniune Europeană. Alegerile europene nu sunt doar o serie de voturi naționale, ci o celebrare colectivă a identității noastre comune și a valorilor democratice. În calitate de cetățeni ai UE, ne reunim pentru a ne modela viitorul comun, depășind frontierele și îmbrățișând puterea diversității noastre. Acest proiect este o dovadă a unității noastre, facilitând participarea fiecărui cetățean, indiferent de locul în care se află în UE, la acest puternic proces democratic.',
+    'line_2' => 'Pentru a susține această viziune a unui proces electoral unificat, Commit Global a creat o platformă, **Votăm pentru EU**, care întruchipează ideea de unitate și colaborare. Această platformă oferă tuturor cetățenilor UE informații esențiale despre cum să își exprime voturile și să găsească rapid secțiile de votare, indiferent de locația lor.',
+    'subtitle' => 'Ce face această platformă specială?',
+    'line_3' => '**Formatul arborelui decizional: ** Platforma utilizează un format de arbore decizional care ghidează cetățenii UE direct către informațiile specifice de care au nevoie.',
+    'line_4' => '**Informații complete: ** Informațiile finale furnizate sunt cuprinzătoare, acoperind cerințele de înregistrare (dacă există), diferite metode de vot (proxy, online, poștă, secție de votare), documentația necesară pentru vot și asistență pentru alegătorii cu dizabilități sau alte limitări.',
+    'line_5' => '**Informații verificate: ** Informațiile prezentate pe site sunt verificate încrucișat din mai multe surse diferite, inclusiv ministerele și organizațiile naționale responsabile cu regulile și procedurile de vot, precum și platforma Parlamentului European.',
+    'line_6' => '**Accesibilitate multilingvă: ** Ne asigurăm că fiecare cetățean al UE poate accesa informațiile de care au nevoie în propria limbă. Platforma este disponibilă în 26 de limbi, arborele decizional al fiecărei țări fiind furnizat în limba (limbile) oficială (oficiale) și în limba engleză. Acest lucru asigură că toată lumea, indiferent de mediul lor lingvistic, poate naviga fără efort în procesul de vot.',
+    'line_7' => '**Orientări interconectate: ** Indiferent de cetățenia UE a alegătorului, de țara de reședință sau de locația sa în ziua alegerilor, platforma oferă instrucțiuni precise și adaptate și informații relevante pentru situația sa unică. Este ca și cum ai avea un asistent de vot personalizat, care face procesul lin și inspirat.',
+    'line_8' => 'Votăm pentru UE este o platformă creată și gestionată de **Commit Global**, primul ONG global de tehnologie civică din lume. Cu sediul central la Haga (Olanda), Commit Global consolidează, întreține și oferă acces gratuit la tehnologia digitală critică, **Infrastructura pentru Bună**, organizațiilor societății civile și populațiilor afectate din întreaga lume. Pentru informații suplimentare despre activitatea noastră, vă rugăm să vizitați [site-ul nostru] (https://www.commitglobal.org).',
 ];
