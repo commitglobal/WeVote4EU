578f7bcf16286309f2bfe9f27c0d2904d9be690d
Amazon Translate <noreply-mt-aws@weblate.org>
2024-05-23 13:10:49 +0000

    Translated using Weblate (Maltese)
    Currently translated at 100.0% (30 of 30 strings)
    
    Translation: WeVote4Eu/terms
    Translate-URL: https://translate.commitglobal.org/projects/wevote4eu/terms/mt/

--- lang/mt/terms.php
+++ lang/mt/terms.php
@@ -1,1 +1,37 @@
 <?php
+return [
+    'intro' => 'It-Termini u l-Kundizzjonijiet Ġenerali tal-Użu jistabbilixxu t-termini u l-kundizzjonijiet ġenerali tal-użu tal-websajt wevote4.eu minn viżitaturi jew benefiċjarji potenzjali.',
+    'subtitle_2' => '2. Dejta personali',
+    'title' => 'Termini u Kundizzjonijiet',
+    'subtitle_1' => '1. L-għan tal-Pjattaforma',
+    'line_1' => 'L-għan ta\' wevote4.eu huwa li tipprovdi informazzjoni rigward il-proċess tal-votazzjoni għall-elezzjonijiet Ewropej.',
+    'subtitle_8' => '8. Bidliet fit-Termini Ġenerali tas-Sit u l-Kundizzjonijiet tal-Użu',
+    'line_7_3' => 'Commit Global jintrabat li ma jiżvelax dejta personali rigward iż-żjarat tiegħek lil wevote4.eu, ħlief għal sitwazzjonijiet legali jew is-sitwazzjonijiet ippreżentati f\'dan id-dokument tat-Termini u l-Kundizzjonijiet.',
+    'line_6_1' => 'Bħala utent tal-websajt wevote4.eu għandek id-drittijiet li ġejjin:',
+    'line_5' => 'Commit Global mhux se jipprovdi dejta personali lil partijiet terzi.',
+    'line_8_3' => 'Huwa strettament ipprojbit li tuża dan is-Sit għall-iskop li jeqirdu jew jinbidlu, il-kontenut tiegħu jew is-sigurtà tiegħu jew għal diskreditar jew fastidju wevote4.eu jew l-organizzazzjoni Commit Global u l-membri tagħha.',
+    'subtitle_4' => '4. It-tul tal-ħażna tad-dejta',
+    'line_8_7' => 'Jekk għandek xi mistoqsijiet rigward l-informazzjoni li tinsab f\'din il-paġna, jekk jogħġbok ikteb lilna fuq info@commitglobal.org.',
+    'line_7_1' => 'Il-protezzjoni tal-informazzjoni waqt l-ipproċessar tad-dejta personali tiegħek hija tħassib ewlieni ta\' Commit Global, għalhekk id-dejta personali kollha miġbura waqt iż-żjarat f\'dan is-sit hija pproċessata skont id-dispożizzjonijiet legali fis-seħħ fir-Rumanija u l-Olanda. Commit Global juża teknoloġiji l-aktar avvanzati u jieħu l-miżuri tekniċi raġonevoli kollha għat-trażmissjoni u l-ħażna tad-dejta personali taħt kundizzjonijiet ta\' sigurtà u kunfidenzjalità sħiħa.',
+    'line_3' => 'Commit Global tipproċessa l-informazzjoni kollha deskritta hawn fuq, fuq Paragrafu 2 “Data personali” li tintuża biex tkompli ttejjeb l-esperjenza tan-navigazzjoni fuq il-websajt tagħna u biex tifhem il-preferenzi tal-viżitaturi tagħna. Aħna mhux se niġbru l-ebda informazzjoni personali permezz ta \'din il-websajt mingħajr il-kunsens espress tiegħek.',
+    'subtitle_6' => '6. Id-drittijiet tiegħek',
+    'line_2' => 'Malli nżuru wevote4.eu aħna awtomatikament niġbru u naħżnu dejta dwar iż-żjara attwali tiegħek. Din l-informazzjoni mhix identifikabbli personalment. L-informazzjoni li nistgħu niġbru hija: sistema operattiva fuq it-tagħmir tiegħek, il-browser li qed tuża, paġni f\'wevote4.eu li żort, is-sors taż-żjara tiegħek (ex: Google, Facebook), id-data u l-ħin taż-żjara tiegħek u d-dominju minn fejn taċċessa l-internet.',
+    'line_4' => 'Id-dejta personali pprovduta tinħażen temporanjament matul it-tħaddim tal-pjattaforma wevote4.eu.',
+    'line_6_3' => 'Tista\' tissottometti talba biex id-dejta personali tiegħek tiġi modifikata jew imħassra fi kwalunkwe mument fiż-żmien, billi tibgħat talba sempliċi permezz ta\' email fuq: info@commitglobal.org',
+    'line_7_2' => 'Id-dejta personali kollha fuq is-sit hija maħżuna u pproċessata fuq servers li jinsabu fl-Unjoni Ewropea, li jaqgħu taħt il-leġiżlazzjoni Ewropea dwar il-protezzjoni tad-dejta personali. L-ebda informazzjoni mogħtija fuq dan is-sit ma tħalli mit-territorju tal-Unjoni Ewropea.',
+    'line_8_2' => 'Id-drittijiet kollha huma riżervati.',
+    'line_8_6' => 'Commit Global jirriserva d-dritt li tirrevedi u taġġorna l-\ "Termini Ġenerali u Kundizzjonijiet ta \'Użu\” fi kwalunkwe ħin, mingħajr avviż jew aċċettazzjoni minn qabel tal-utenti.',
+    'line_7_4' => 'Jekk niskopru inċident rigward is-sigurtà tad-dejta personali li joħloq riskju għad-drittijiet u l-libertajiet tal-utenti tagħna, se nnotifikaw lill-Awtorità Nazzjonali Superviżorja għall-Ipproċessar tad-Data Personali fi żmien 72 siegħa. Jekk l-inċident tas-sigurtà x\'aktarx jippreżenta riskju għoli għad-drittijiet u l-libertajiet tiegħek, int tiġi nnotifikat ukoll.',
+    'subtitle_5' => '5. Trasferiment ta\' Dejta Personali lil partijiet terzi',
+    'line_8_1' => 'Id-drittijiet tal-awtur għall-informazzjoni kollha fuq din il-websajt huma proprjetà ta\' Commit Global u l-imsieħba tal-proġett. L-ebda materjal fuq dan is-sit ma jista\' jiġi riprodott parzjalment, kollu kemm hu jew modifikat mingħajr il-kunsens espress espress minn Commit Global, id-detentur ta\' dan id-dritt.',
+    'subtitle_3' => '3. Għan u raġunijiet',
+    'line_8_8' => 'Din il-verżjoni tat-Termini u Kundizzjonijiet wevote4.eu hija effettiva mit-23 ta\' Mejju 2024.',
+    'line_8_5' => 'Il-\ "Termini u Kundizzjonijiet Ġenerali ta \'Użu\” jikkostitwixxu, fl-intier tagħhom, ftehim konkluż bejnek u Commit Global rigward l-użu ta\' dan is-sit.',
+    'line_6_2' => '- Id-dritt għal informazzjoni rigward l-ipproċessar ta\' data personali minn Commit Global;
+- Id-dritt għal aċċess għad-dejta personali, fuq talba u mingħajr ħlas għal talba waħda fis-sena;
+- Id-dritt ta\' intervent, rettifika, tħassir jew portabbiltà tad-data personali, fuq talba u mingħajr ħlas;
+- Id-dritt li tindirizza kwalunkwe awtorità kompetenti rigward kwistjonijiet dwar il-privatezza tad-dejta b\'rabta mal-użu tad-dejta personali tiegħek
+- Id-dritt għall-oppożizzjoni u l-proċess awtomatizzat tat-teħid tad-deċiżjonijiet individwali kif ukoll id-dritt li jiġi ristrett l-ipproċessar.',
+    'subtitle_7' => '7. Trademark u Copyright',
+    'line_8_4' => 'Commit Global se japplika l-miżuri tekniċi u organizzattivi kollha ta \'sigurtà biex jipproteġi d-dejta li fuqha għandha kontroll kontra kwalunkwe sitwazzjoni ta\' manipulazzjoni aċċidentali jew intenzjonata, telf, qerda jew kontra aċċess minn persuna mhux awtorizzata.',
+];
