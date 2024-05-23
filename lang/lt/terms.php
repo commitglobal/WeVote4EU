e5590a5967d334d1fe5aba780b257a229e64e7bd
Amazon Translate <noreply-mt-aws@weblate.org>
2024-05-23 13:06:57 +0000

    Translated using Weblate (Lithuanian)
    Currently translated at 100.0% (30 of 30 strings)
    
    Translation: WeVote4Eu/terms
    Translate-URL: https://translate.commitglobal.org/projects/wevote4eu/terms/lt/

--- lang/lt/terms.php
+++ lang/lt/terms.php
@@ -1,1 +1,37 @@
 <?php
+return [
+    'line_8_6' => '“Commit Global” pasilieka teisę bet kuriuo metu peržiūrėti ir atnaujinti “Bendrąsias naudojimo sąlygas\”, be išankstinio vartotojų įspėjimo ar sutikimo.',
+    'line_7_4' => 'Jei aptiksime incidentą dėl asmens duomenų saugumo, kuris kelia pavojų mūsų vartotojų teisėms ir laisvėms, per 72 valandas apie tai pranešsime Nacionalinei asmens duomenų tvarkymo priežiūros institucijai. Jei saugumo incidentas gali kelti didelį pavojų jūsų teisėms ir laisvėms, jums taip pat bus pranešta.',
+    'subtitle_5' => '5. Asmens duomenų perdavimas tretiesiems asmenims',
+    'line_8_1' => 'Visos informacijos šioje svetainėje autorių teisės priklauso “Commit Global” ir projekto partneriams. Jokia šioje svetainėje esanti medžiaga negali būti atgaminta iš dalies, visa ar modifikuota be aiškaus sutikimo, išreikšto Commit Global, šios teisės turėtojo.',
+    'line_8_8' => 'Ši wevote4.eu taisyklių ir sąlygų versija įsigalioja nuo 2024 m. gegužės 23 d.',
+    'line_8_5' => 'Bendrosios naudojimo sąlygos yra visos jūsų ir “Commit Global” sudarytas susitarimas dėl naudojimosi šia svetaine.',
+    'line_7_3' => 'Commit Global įsipareigoja neatskleisti asmens duomenų apie jūsų apsilankymus wevote4.eu, išskyrus teisines situacijas ar situacijas, pateiktas šiame Taisyklių ir sąlygų dokumente.',
+    'subtitle_4' => '4. Duomenų saugojimo trukmė',
+    'intro' => 'Bendrosiose naudojimosi taisyklėse ir sąlygose nustatytos galimų lankytojų ar naudos gavėjų bendrieji wevote4.eu interneto svetainės naudojimo terminai ir sąlygos.',
+    'subtitle_2' => '2. Asmens duomenys',
+    'line_3' => '“Commit Global” tvarko visą aukščiau aprašytą informaciją 2 dalyje “Asmens duomenys”, kuri naudojama siekiant toliau pagerinti naršymo patirtį mūsų svetainėje ir suprasti mūsų lankytojų pageidavimus. Mes nerinksime jokios asmeninės informacijos per šią svetainę be aiškaus jūsų sutikimo.',
+    'line_4' => 'Pateikti asmens duomenys yra laikinai saugomi wevote4.eu platformos veikimo metu.',
+    'title' => 'Taisyklės ir sąlygos',
+    'subtitle_3' => '3. Tikslas ir pagrindai',
+    'subtitle_1' => '1. Platformos tikslas',
+    'subtitle_6' => '6. Jūsų teisės',
+    'line_6_1' => 'Kaip wevote4.eu svetainės naudotojas turite šias teises:',
+    'line_5' => '“Commit Global” neteiks asmens duomenų trečiosioms šalims.',
+    'line_8_3' => 'Griežtai draudžiama naudoti šią Svetainę siekiant sunaikinti ar pakeisti ją, jos turinį ar jos saugumą arba diskredituoti ar priekabiauti wevote4.eu ar Commit Global organizacijos ir jos narių.',
+    'line_8_7' => 'Jei turite klausimų dėl šiame puslapyje pateiktos informacijos, rašykite mums adresu info@commitglobal.org.',
+    'subtitle_8' => '8. Bendrųjų svetainės naudojimo sąlygų pakeitimai',
+    'line_7_1' => 'Informacijos apsauga tvarkant Jūsų asmens duomenis yra didelis “Commit Global” rūpestis, todėl visi asmens duomenys, surinkti apsilankymų šioje svetainėje metu, tvarkomi pagal Rumunijoje ir Nyderlanduose galiojančias teisines nuostatas. “Commit Global” naudoja moderniausias technologijas ir imasi visų pagrįstų techninių priemonių asmens duomenų perdavimui ir saugojimui visiško saugumo ir konfidencialumo sąlygomis.',
+    'line_2' => 'Apsilankę wevote4.eu mes automatiškai rinksime ir saugosime duomenis apie jūsų dabartinį apsilankymą. Ši informacija nėra asmens tapatybė. Informacija, kurią galime rinkti, yra: jūsų įrenginio operacinė sistema, jūsų naudojama naršyklė, svetainės wevote4.eu puslapiai, kuriuose lankėtės, jūsų apsilankymo šaltinis (pvz.: “Google”, “Facebook”), apsilankymo data ir laikas bei domenas, iš kurio naudojatės internetu.',
+    'line_6_3' => 'Prašymą pakeisti ar ištrinti Jūsų asmens duomenis galite pateikti bet kuriuo metu, atsiųsdami paprastą užklausą el. paštu adresu: info@commitglobal.org',
+    'line_7_2' => 'Visi svetainėje esantys asmens duomenys yra saugomi ir tvarkomi Europos Sąjungoje esančiuose serveriuose, kurie patenka į Europos teisės aktus dėl asmens duomenų apsaugos. Jokia šioje svetainėje pateikta informacija nepalieka Europos Sąjungos teritorijos.',
+    'line_8_2' => 'Visos teisės saugomos.',
+    'line_6_2' => '- Teisė į informaciją apie “Commit Global” vykdomą asmens duomenų tvarkymą;
+- Teisė susipažinti su asmens duomenimis, paprašius ir nemokamai už vieną prašymą per metus;
+- Asmens duomenų įsikišimo, ištaisymo, ištrynimo ar perkeliamumo teisė, gavus prašymą ir nemokamai;
+- Teisė kreiptis į bet kurią kompetentingą instituciją dėl duomenų privatumo klausimų, susijusių su jūsų asmens duomenų naudojimu
+- Teisė į protestą ir automatizuotas individualaus sprendimų priėmimo procesas, taip pat teisė apriboti duomenų tvarkymą.',
+    'line_1' => '“wevote4.eu” tikslas — pateikti informaciją apie Europos Parlamento rinkimų balsavimo procesą.',
+    'subtitle_7' => '7. Prekės ženklas ir autorių teisės',
+    'line_8_4' => '“Commit Global” taikys visas technines ir organizacines saugumo priemones, kad apsaugotų duomenis, kuriuos kontroliuoja, nuo bet kokios atsitiktinės ar tyčinės manipuliacijos, praradimo, sunaikinimo ar pašalinio asmens prieigos.',
+];
