7d46814d0e64cedcfb64caa20a4df578e25f6699
Amazon Translate <noreply-mt-aws@weblate.org>
2024-05-23 13:07:50 +0000

    Translated using Weblate (Polish)
    Currently translated at 100.0% (30 of 30 strings)
    
    Translation: WeVote4Eu/terms
    Translate-URL: https://translate.commitglobal.org/projects/wevote4eu/terms/pl/

--- lang/pl/terms.php
+++ lang/pl/terms.php
@@ -1,1 +1,37 @@
 <?php
+return [
+    'intro' => 'Ogólne Warunki Użytkowania określają ogólne warunki korzystania ze strony internetowej wevote4.eu przez potencjalnych odwiedzających lub beneficjentów.',
+    'subtitle_2' => '2. Dane osobowe',
+    'line_2' => 'Po odwiedzeniu strony wevote4.eu będziemy automatycznie zbierać i przechowywać dane o Twojej bieżącej wizycie. Informacje te nie są identyfikowalne. Informacje, które możemy gromadzić, to: system operacyjny na urządzeniu użytkownika, przeglądarka, z której korzystasz, strony w witrynie wevote4.eu, które odwiedzasz, źródło Twojej wizyty (np. Google, Facebook), data i godzina wizyty oraz domena, z której uzyskujesz dostęp do Internetu.',
+    'title' => 'Regulamin',
+    'subtitle_3' => '3. Cel i podstawy',
+    'subtitle_1' => '1. Cel Platformy',
+    'line_6_1' => 'Jako użytkownik strony internetowej wevote4.eu przysługują Ci następujące prawa:',
+    'line_5' => 'Commit Global nie przekaże danych osobowych stronom trzecim.',
+    'line_8_3' => 'Surowo zabrania się korzystania z tej Witryny w celu zniszczenia lub zmiany jej treści lub bezpieczeństwa lub w celu dyskredytowania lub nękania wevote4.eu lub organizacji Commit Global i jej członków.',
+    'subtitle_4' => '4. Czas przechowywania danych',
+    'line_8_7' => 'Jeśli masz jakiekolwiek pytania dotyczące informacji zawartych na tej stronie, napisz do nas na adres info@commitglobal.org.',
+    'subtitle_8' => '8. Zmiany w Ogólnych Warunkach Korzystania z Witryny',
+    'line_7_1' => 'Ochrona informacji podczas przetwarzania Twoich danych osobowych jest głównym problemem Commit Global, dlatego wszystkie dane osobowe gromadzone podczas wizyt na tej stronie są przetwarzane zgodnie z przepisami prawa obowiązującymi w Rumunii i Holandii. Commit Global wykorzystuje najnowocześniejsze technologie i podejmuje wszelkie uzasadnione środki techniczne w celu przekazywania i przechowywania danych osobowych w warunkach pełnego bezpieczeństwa i poufności.',
+    'line_3' => 'Commit Global przetwarza wszystkie informacje opisane powyżej, w punkcie 2 „Dane osobowe”, które są wykorzystywane do dalszej poprawy nawigacji na naszej stronie internetowej i zrozumienia preferencji naszych odwiedzających. Nie będziemy zbierać żadnych danych osobowych za pośrednictwem tej strony internetowej bez Twojej wyraźnej zgody.',
+    'subtitle_6' => '6. Twoje prawa',
+    'line_4' => 'Podane dane osobowe są tymczasowo przechowywane w trakcie działania platformy wevote4.eu.',
+    'line_6_3' => 'Możesz złożyć wniosek o modyfikację lub usunięcie swoich danych osobowych w dowolnym momencie, wysyłając proste żądanie pocztą elektroniczną na adres: info@commitglobal.org',
+    'line_7_2' => 'Wszystkie dane osobowe na stronie są przechowywane i przetwarzane na serwerach znajdujących się w Unii Europejskiej, które podlegają prawodawstwu europejskiemu dotyczącemu ochrony danych osobowych. Żadne informacje podane na tej stronie nie opuszczają terytorium Unii Europejskiej.',
+    'line_8_2' => 'Wszelkie prawa są zastrzeżone.',
+    'line_8_6' => 'Commit Global zastrzega sobie prawo do zmiany i aktualizacji „Ogólnych Warunków Użytkowania” w dowolnym momencie, bez powiadomienia lub uprzedniej akceptacji użytkowników.',
+    'line_7_4' => 'W przypadku stwierdzenia incydentu związanego z bezpieczeństwem danych osobowych, który stanowi zagrożenie dla praw i wolności naszych użytkowników, powiadomimy o tym Krajowy Organ Nadzoru ds. Przetwarzania Danych Osobowych w ciągu 72 godzin. Jeśli incydent związany z bezpieczeństwem może stanowić wysokie ryzyko dla Twoich praw i wolności, zostaniesz również powiadomiony.',
+    'subtitle_5' => '5. Przekazywanie danych osobowych stronom trzecim',
+    'line_8_1' => 'Prawa autorskie do wszystkich informacji zawartych w tej witrynie są własnością Commit Global i partnerów projektu. Żadne materiały na tej stronie nie mogą być częściowo, w całości lub modyfikowane bez wyraźnej zgody wyrażonej przez Commit Global, posiadacza tego prawa.',
+    'line_8_8' => 'Niniejsza wersja Regulaminu wevote4.eu wchodzi w życie z dniem 23 maja 2024 r.',
+    'line_8_5' => 'Ogólne Warunki Użytkowania stanowią w całości umowę zawartą między Tobą a Commit Global dotyczącą korzystania z tej strony.',
+    'line_7_3' => 'Commit Global zobowiązuje się nie ujawniać danych osobowych dotyczących wizyt na wevote4.eu, z wyjątkiem sytuacji prawnych lub sytuacji przedstawionych w niniejszym dokumencie Regulaminu.',
+    'line_6_2' => '- Prawo do informacji dotyczących przetwarzania danych osobowych przez Commit Global;
+- Prawo dostępu do danych osobowych, na żądanie i bezpłatnie na jedno żądanie rocznie;
+- prawo do interwencji, sprostowania, usunięcia lub przenoszenia danych osobowych, na żądanie i bezpłatnie;
+- Prawo do zwrócenia się do każdego właściwego organu w sprawie kwestii prywatności danych w związku z wykorzystaniem Twoich danych osobowych
+- Prawo do sprzeciwu i zautomatyzowany indywidualny proces decyzyjny oraz prawo do ograniczenia przetwarzania.',
+    'line_1' => 'Celem wevote4.eu jest dostarczanie informacji dotyczących procesu głosowania w wyborach europejskich.',
+    'subtitle_7' => '7. Znak towarowy i prawa autorskie',
+    'line_8_4' => 'Commit Global zastosuje wszelkie techniczne i organizacyjne środki bezpieczeństwa w celu ochrony danych, nad którymi ma kontrolę, przed każdą sytuacją przypadkowej lub celowej manipulacji, utraty, zniszczenia lub przed dostępem osób nieupoważnionych.',
+];
