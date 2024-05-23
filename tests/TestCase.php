a02f1d6ab2d475f5595aff7058f30ddc9c2256a3
Andrei Ioniță <hi@andrei.io>
2024-05-23 19:08:23 +0300

    add test

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -8,5 +8,34 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    public static function languages(): array
+    {
+        return [
+            'bulgarian' => ['bg'],
+            'croatian' => ['hr'],
+            'czech' => ['cs'],
+            'danish' => ['da'],
+            'dutch' => ['nl'],
+            'english' => ['en'],
+            'estonian' => ['et'],
+            'finnish' => ['fi'],
+            'french' => ['fr'],
+            'german' => ['de'],
+            'greek' => ['el'],
+            'hungarian' => ['hu'],
+            'irish' => ['ga'],
+            'italian' => ['it'],
+            'luxembourgish' => ['lb'],
+            'latvian' => ['lv'],
+            'lithuanian' => ['lt'],
+            'maltese' => ['mt'],
+            'polish' => ['pl'],
+            'portuguese' => ['pt'],
+            'romanian' => ['ro'],
+            'slovak' => ['sk'],
+            'slovene' => ['sl'],
+            'spanish' => ['es'],
+            'swedish' => ['sv'],
+        ];
+    }
 }
