aa0f5e199bbb15cc1f7218f906ff98d739a48ddb
Weblate Translation Memory <noreply-mt-weblate-translation-memory@weblate.org>
2024-05-23 13:07:45 +0000

    Translated using Weblate (Latvian)
    Currently translated at 100.0% (30 of 30 strings)
    
    Translation: WeVote4Eu/terms
    Translate-URL: https://translate.commitglobal.org/projects/wevote4eu/terms/lv/

--- lang/lv/terms.php
+++ lang/lv/terms.php
@@ -1,1 +1,9 @@
 <?php
+return [
+    'line_6_1' => 'Jums kā wevote4.eu tīmekļa vietnes lietotājam ir šādas tiesības:',
+    'line_6_2' => '- Tiesības uz informāciju par personas datu apstrādi, ko veic Commit Global;
+- Tiesības piekļūt personas datiem pēc pieprasījuma un bez maksas par vienu pieprasījumu gadā;
+- tiesības iejaukties, labot, dzēst vai pārnēsāt personas datus pēc pieprasījuma un bez maksas;
+- Tiesības vērsties jebkurai kompetentajai iestādei attiecībā uz datu privātuma jautājumiem saistībā ar jūsu personas datu izmantošanu
+- Tiesības iebilst un automatizēts individuālais lēmumu pieņemšanas process, kā arī tiesības ierobežot apstrādi.',
+];
