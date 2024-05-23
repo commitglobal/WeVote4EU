b4a56b49927079c98d14aafa3611194b5e49e44a
Amazon Translate <noreply-mt-aws@weblate.org>
2024-05-23 13:08:20 +0000

    Translated using Weblate (Turkish)
    Currently translated at 100.0% (30 of 30 strings)
    
    Translation: WeVote4Eu/terms
    Translate-URL: https://translate.commitglobal.org/projects/wevote4eu/terms/tr/

--- lang/tr/terms.php
+++ lang/tr/terms.php
@@ -1,1 +1,37 @@
 <?php
+return [
+    'subtitle_2' => '2. Kişisel veriler',
+    'line_6_1' => 'wevote4.eu web sitesinin bir kullanıcısı olarak aşağıdaki haklara sahipsiniz:',
+    'subtitle_8' => '8. Genel Site Kullanım Şartları ve Koşullarında Yapılan Değişiklikler',
+    'line_7_3' => 'Commit Global, yasal durumlar veya bu Hüküm ve Koşullar belgesinde sunulan durumlar dışında, wevote4.eu ziyaretlerinizle ilgili kişisel verileri ifşa etmemeyi taahhüt eder.',
+    'subtitle_7' => '7. Marka ve Telif Hakkı',
+    'line_8_2' => 'Tüm hakları saklıdır.',
+    'line_8_7' => 'Bu sayfada yer alan bilgilerle ilgili herhangi bir sorunuz varsa, lütfen info@commitglobal.org adresinden bize yazın.',
+    'intro' => 'Genel Kullanım Şartları ve Koşulları, wevote4.eu web sitesinin potansiyel ziyaretçiler veya yararlanıcılar tarafından kullanım genel hüküm ve koşullarını belirler.',
+    'line_5' => 'Commit Global üçüncü şahıslara kişisel veri sağlamayacaktır.',
+    'line_8_3' => 'Bu Siteyi, içeriğini veya güvenliğini yok etmek veya değiştirmek veya wevote4.eu veya Commit Global organizasyonunu ve üyelerini gözden düşürmek veya taciz etmek amacıyla kullanmak kesinlikle yasaktır.',
+    'subtitle_4' => '4. Veri depolama süresi',
+    'line_7_1' => 'Kişisel verilerinizin işlenmesi sırasında bilgilerin korunması Commit Global\'in önemli bir endişesidir, bu nedenle bu siteye yapılan ziyaretler sırasında toplanan tüm kişisel veriler Romanya ve Hollanda\'da yürürlükte olan yasal hükümlere göre işlenir. Commit Global, en son teknolojileri kullanır ve kişisel verilerin tam güvenlik ve gizlilik koşulları altında iletilmesi ve depolanması için tüm makul teknik önlemleri alır.',
+    'line_3' => 'Commit Global, web sitemizdeki gezinme deneyimini daha da iyileştirmek ve ziyaretçilerimizin tercihlerini anlamak için kullanılan Paragraf 2 “Kişisel veriler” ile ilgili yukarıda açıklanan tüm bilgileri işler. Açık izniniz olmadan bu web sitesi aracılığıyla herhangi bir kişisel bilgi toplamayacağız.',
+    'subtitle_6' => '6. Haklarınız',
+    'line_2' => 'wevote4.eu\'yu ziyaret ettiğinizde, mevcut ziyaretinizle ilgili verileri otomatik olarak toplayacağız ve saklayacağız. Bu bilgiler kişisel olarak tanımlanamaz. Toplayabileceğimiz bilgiler şunlardır: cihazınızdaki işletim sistemi, kullandığınız tarayıcı, wevote4.eu\'da ziyaret ettiğiniz sayfalar, ziyaretinizin kaynağı (örn. Google, Facebook), ziyaretinizin tarihi ve saati ve internete eriştiğiniz alan adıdır.',
+    'line_4' => 'Sağlanan kişisel veriler, wevote4.eu platformunun çalışması sırasında geçici olarak saklanır.',
+    'title' => 'Şartlar ve Koşullar',
+    'line_6_3' => 'Kişisel verilerinizin değiştirilmesi veya silinmesi için istediğiniz zaman, e-posta yoluyla basit bir istek göndererek: info@commitglobal.org',
+    'line_7_2' => 'Sitedeki tüm kişisel veriler, kişisel verilerin korunmasına ilişkin Avrupa mevzuatına giren Avrupa Birliği\'nde bulunan sunucularda saklanır ve işlenir. Bu sitede sağlanan hiçbir bilgi Avrupa Birliği topraklarını terk etmez.',
+    'line_8_6' => 'Commit Global, “Genel Kullanım Şartları ve Koşullarını” istediği zaman, önceden haber vermeksizin veya kullanıcıların kabulü olmaksızın revize etme ve güncelleme hakkını saklı tutar.',
+    'line_7_4' => 'Kullanıcılarımızın hak ve özgürlükleri için risk oluşturan kişisel verilerin güvenliği ile ilgili bir olay tespit edersek, 72 saat içinde Kişisel Verilerin İşlenmesi için Ulusal Denetim Kurumu\'nu bilgilendireceğiz. Güvenlik olayının hak ve özgürlükleriniz için yüksek bir risk oluşturması muhtemel ise, ayrıca bilgilendirileceksiniz.',
+    'subtitle_5' => '5. Kişisel Verilerin Üçüncü Şahıslara Aktarılması',
+    'line_8_1' => 'Bu web sitesindeki tüm bilgilerin telif hakkı Commit Global ve proje ortaklarına aittir.. Bu sitedeki hiçbir materyal, bu hakkın sahibi olan Commit Global tarafından ifade edilen açık rıza olmadan kısmen, tamamen çoğaltılamaz veya değiştirilemez.',
+    'subtitle_3' => '3. Amaç ve gerekçeler',
+    'line_8_8' => 'wevote4.eu Hüküm ve Koşullarının bu sürümü 23 Mayıs 2024 tarihinden itibaren geçerlidir.',
+    'line_8_5' => '“Genel Kullanım Şartları ve Koşulları”, bütünüyle, bu sitenin kullanımıyla ilgili olarak siz ve Commit Global arasında imzalanan bir anlaşmayı teşkil eder.',
+    'line_6_2' => '- Kişisel verilerin Commit Global tarafından işlenmesine ilişkin bilgi edinme hakkı;
+- Talep üzerine ve yılda bir talep için ücretsiz kişisel verilere erişim hakkı;
+- Talep üzerine ve ücretsiz olarak kişisel verilerin müdahale, düzeltilmesi, silinmesi veya taşınabilirliği hakkı;
+- Kişisel verilerinizin kullanımıyla ilgili veri gizliliği konularında herhangi bir yetkili makama başvurma hakkı
+- İtiraz hakkı ve otomatik bireysel karar verme sürecinin yanı sıra işlemeyi kısıtlama hakkı.',
+    'subtitle_1' => '1. Platformun Amacı',
+    'line_1' => 'wevote4.eu\'nun amacı, Avrupa seçimleri için oylama süreci hakkında bilgi sağlamaktır.',
+    'line_8_4' => 'Commit Global, üzerinde kontrol sahibi olduğu verileri kazara veya kasıtlı manipülasyon, kayıp, imha veya yetkisiz bir kişinin erişimine karşı korumak için tüm teknik ve organizasyonel güvenlik önlemlerini uygulayacaktır.',
+];
