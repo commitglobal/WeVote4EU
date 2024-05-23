cc7b8fee4ef22a7638735ee6567039816af58fbc
Amazon Translate <noreply-mt-aws@weblate.org>
2024-05-23 13:07:35 +0000

    Translated using Weblate (Finnish)
    Currently translated at 100.0% (30 of 30 strings)
    
    Translation: WeVote4Eu/terms
    Translate-URL: https://translate.commitglobal.org/projects/wevote4eu/terms/fi/

--- lang/fi/terms.php
+++ lang/fi/terms.php
@@ -1,1 +1,37 @@
 <?php
+return [
+    'line_8_3' => 'On ehdottomasti kiellettyä käyttää tätä sivustoa sen, sen sisällön tai turvallisuuden tuhoamiseen tai muuttamiseen tai wevote4.eu:n tai Commit Global -organisaation ja sen jäsenten diskreditoimiseen tai häiritsemiseen.',
+    'subtitle_4' => '4. Tietojen tallennuksen kesto',
+    'line_8_7' => 'Jos sinulla on kysyttävää tämän sivun tiedoista, kirjoita meille osoitteeseen info@commitglobal.org.',
+    'intro' => 'Yleiset käyttöehdot määrittelevät yleiset ehdot wevote4.eu-verkkosivuston mahdollisille vierailijoille tai edunsaajille.',
+    'subtitle_2' => '2. Henkilötiedot',
+    'line_7_3' => 'Commit Global sitoutuu olemaan luovuttamatta henkilötietoja wevote4.eu-sivustolla vierailuistasi lukuun ottamatta oikeudellisia tilanteita tai tässä käyttöehtoasiakirjassa esitettyjä tilanteita.',
+    'line_6_1' => 'Wevote4.eu-sivuston käyttäjänä sinulla on seuraavat oikeudet:',
+    'line_5' => 'Commit Global ei luovuta henkilötietoja kolmansille osapuolille.',
+    'subtitle_8' => '8. Muutokset sivuston yleisiin käyttöehtoihin',
+    'line_7_1' => 'Tietojen suojaaminen henkilötietojesi käsittelyn aikana on Commit Globalin suuri huolenaihe, joten kaikkia tällä sivustolla vierailujen aikana kerättyjä henkilötietoja käsitellään Romaniassa ja Alankomaissa voimassa olevien lakisääteisten määräysten mukaisesti. Commit Global käyttää huipputeknologiaa ja toteuttaa kaikki kohtuulliset tekniset toimenpiteet henkilötietojen siirtämiseksi ja tallentamiseksi täydellisen turvallisuuden ja luottamuksellisuuden olosuhteissa.',
+    'line_3' => 'Commit Global käsittelee kaikki edellä kuvatut tiedot kohdassa 2 ”Henkilötiedot”, joita käytetään verkkosivustomme navigointikokemuksen parantamiseen ja kävijöiden mieltymysten ymmärtämiseen. Emme kerää mitään henkilökohtaisia tietoja tämän verkkosivuston kautta ilman nimenomaista suostumustasi.',
+    'subtitle_6' => '6. Sinun oikeutesi',
+    'line_2' => 'Vieraillessamme wevote4.eu keräämme ja tallennamme automaattisesti tietoja nykyisestä vierailustasi. Nämä tiedot eivät ole henkilökohtaisesti tunnistettavissa. Voimme kerätä tietoja: laitteesi käyttöjärjestelmä, käyttämäsi selain, wevote4.eu-sivuilla vieraillut sivut, vierailusi lähde (esim. Google, Facebook), vierailusi päivämäärä ja kellonaika sekä verkkotunnus, josta käytät Internetiä.',
+    'line_4' => 'Annetut henkilötiedot tallennetaan väliaikaisesti wevote4.eu-alustan toiminnan aikana.',
+    'title' => 'Säännöt ja ehdot',
+    'line_6_3' => 'Voit pyytää henkilötietojesi muuttamista tai poistamista milloin tahansa lähettämällä yksinkertaisen pyynnön sähköpostitse osoitteeseen: info@commitglobal.org',
+    'line_7_2' => 'Kaikki sivuston henkilötiedot tallennetaan ja käsitellään Euroopan unionissa sijaitsevilla palvelimilla, jotka kuuluvat henkilötietojen suojaa koskevan eurooppalaisen lainsäädännön piiriin. Mikään tällä sivustolla annettu tieto ei poistu Euroopan unionin alueelta.',
+    'line_8_2' => 'Kaikki oikeudet pidätetään.',
+    'line_8_6' => 'Commit Global pidättää oikeuden tarkistaa ja päivittää ”Yleisiä käyttöehtoja” milloin tahansa ilman erillistä ilmoitusta tai käyttäjien ennakkohyväksyntää.',
+    'line_7_4' => 'Jos havaitsemme henkilötietojen turvallisuuteen liittyvän tapahtuman, joka aiheuttaa riskin käyttäjiemme oikeuksille ja vapauksille, ilmoitamme siitä henkilötietojen käsittelyä käsittelevälle kansalliselle valvontaviranomaiselle 72 tunnin kuluessa. Jos turvallisuushäiriö todennäköisesti aiheuttaa suuren riskin oikeuksille ja vapauksille, sinulle ilmoitetaan myös siitä.',
+    'subtitle_5' => '5. Henkilötietojen siirtäminen kolmansille osapuolille',
+    'line_8_1' => 'Kaikkien tämän verkkosivuston tietojen tekijänoikeudet omistavat Commit Global ja projektin kumppanit. Mitään tämän sivuston materiaalia ei voida jäljentää osittain, kokonaan tai muokata ilman tämän oikeuden haltijan Commit Globalin nimenomaista suostumusta.',
+    'subtitle_3' => '3. Tarkoitus ja perusteet',
+    'line_8_8' => 'Tämä wevote4.eu-käyttöehtojen versio on voimassa 23. toukokuuta 2024 alkaen.',
+    'line_8_5' => '”Yleiset käyttöehdot” muodostavat kokonaisuudessaan sinun ja Commit Globalin välisen sopimuksen tämän sivuston käytöstä.',
+    'line_6_2' => '- Oikeus saada tietoja henkilötietojen käsittelystä Commit Globalin toimesta;
+- oikeus saada pääsy henkilötietoihin pyynnöstä ja maksutta yhden pyynnön osalta vuodessa;
+- oikeus puuttua asiaan, oikaisuun, poistamiseen tai siirrettävyyteen pyynnöstä ja maksutta;
+- Oikeus kääntyä mihin tahansa toimivaltaiseen viranomaiseen liittyen henkilötietojesi käyttöön liittyvissä tietosuojaan liittyvissä kysymyksissä
+- Vastustamisoikeus ja automatisoitu yksittäinen päätöksentekoprosessi sekä oikeus rajoittaa käsittelyä.',
+    'subtitle_1' => '1. Foorumin tavoite',
+    'line_1' => 'wevote4.eu-sivuston tavoitteena on antaa tietoa Euroopan parlamentin vaalien äänestysprosessista.',
+    'subtitle_7' => '7. Tavaramerkki ja tekijänoik',
+    'line_8_4' => 'Commit Global soveltaa kaikkia teknisiä ja organisatorisia turvatoimia suojaamaan hallussaan olevia tietoja tahattomalta tai tahalliselta manipuloinnilta, häviämiseltä, tuhoutumiselta tai luvattomalta henkilöltä.',
+];
