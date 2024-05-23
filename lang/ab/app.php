cf8b0f5cca9ab73380def41a4c0fa9581195c304
Languages add-on <noreply-addon-languages@weblate.org>
2024-05-23 13:05:12 +0000

    Added translation using Weblate (Abkhazian)

---
+++ lang/ab/app.php
@@ -0,0 +1,19 @@
+<?php
+return [
+    'action' => [
+        'back' => '',
+        'home' => '',
+    ],
+    'banner' => '',
+    'select_nationality' => '',
+    'parliament_elections_title' => '',
+    'navigation' => [
+        'home' => '',
+        'about' => '',
+    ],
+    'hero' => [
+        'title' => '',
+        'name' => '',
+        'description' => '',
+    ],
+];
