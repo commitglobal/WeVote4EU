c9666d326ecdd4599dab1232e0e6c0803d17c207
Weblate <noreply@weblate.org>
2024-05-23 13:05:12 +0000

    Update translation files
    Updated by "Remove blank strings" hook in Weblate.
    
    Translation: WeVote4Eu/app
    Translate-URL: https://translate.commitglobal.org/projects/wevote4eu/app/

--- lang/ab/app.php
+++ lang/ab/app.php
@@ -1,19 +1,1 @@
 <?php
-return [
-    'action' => [
-        'back' => '',
-        'home' => '',
-    ],
-    'banner' => '',
-    'select_nationality' => '',
-    'parliament_elections_title' => '',
-    'navigation' => [
-        'home' => '',
-        'about' => '',
-    ],
-    'hero' => [
-        'title' => '',
-        'name' => '',
-        'description' => '',
-    ],
-];
