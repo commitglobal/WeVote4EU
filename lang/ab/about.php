671a5f4b2432db194c42f2223aedba5f0e7a5eaf
Weblate <noreply@weblate.org>
2024-05-23 13:06:14 +0000

    Update translation files
    Updated by "Remove blank strings" hook in Weblate.
    
    Translation: WeVote4Eu/about
    Translate-URL: https://translate.commitglobal.org/projects/wevote4eu/about/

--- lang/ab/about.php
+++ lang/ab/about.php
@@ -1,13 +1,1 @@
 <?php
-return [
-    'title' => '',
-    'line_1' => '',
-    'line_2' => '',
-    'subtitle' => '',
-    'line_3' => '',
-    'line_4' => '',
-    'line_5' => '',
-    'line_6' => '',
-    'line_7' => '',
-    'line_8' => '',
-];
