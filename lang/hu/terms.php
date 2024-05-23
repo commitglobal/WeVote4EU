feeb33b40003e7153d96179b92d7517dcb350b4b
Weblate Translation Memory <noreply-mt-weblate-translation-memory@weblate.org>
2024-05-23 13:10:44 +0000

    Translated using Weblate (Hungarian)
    Currently translated at 100.0% (30 of 30 strings)
    
    Translation: WeVote4Eu/terms
    Translate-URL: https://translate.commitglobal.org/projects/wevote4eu/terms/hu/

--- lang/hu/terms.php
+++ lang/hu/terms.php
@@ -1,1 +1,9 @@
 <?php
+return [
+    'line_6_1' => 'A wevote4.eu weboldal felhasználójaként Ön a következő jogokkal rendelkezik:',
+    'line_6_2' => '- A személyes adatok Commit Global általi feldolgozásával kapcsolatos tájékoztatáshoz való jog;
+- a személyes adatokhoz való hozzáférés joga, kérésre és díjmentesen, évente egy kérelem esetén;
+- a személyes adatok beavatkozásához, helyesbítéséhez, törléséhez vagy hordozhatósághoz való jog kérésre és ingyenesen;
+- Az Ön személyes adatainak felhasználásával összefüggő adatvédelmi kérdésekkel kapcsolatban bármely illetékes hatósághoz fordulni
+- A felszólaláshoz való jog és az automatizált egyéni döntéshozatali folyamat, valamint az adatkezelés korlátozásának joga.',
+];
