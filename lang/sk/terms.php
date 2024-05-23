c62c0a6ba54532cdcfcd5e1ccb8fec323b590dcb
Amazon Translate <noreply-mt-aws@weblate.org>
2024-05-23 13:09:11 +0000

    Translated using Weblate (Slovak)
    Currently translated at 100.0% (30 of 30 strings)
    
    Translation: WeVote4Eu/terms
    Translate-URL: https://translate.commitglobal.org/projects/wevote4eu/terms/sk/

--- lang/sk/terms.php
+++ lang/sk/terms.php
@@ -1,1 +1,37 @@
 <?php
+return [
+    'subtitle_4' => '4. Trvanie ukladania údajov',
+    'line_8_7' => 'Ak máte akékoľvek otázky týkajúce sa informácií obsiahnutých na tejto stránke, napíšte nám na adresu info@commitglobal.org.',
+    'intro' => 'Všeobecné podmienky používania stanovujú všeobecné podmienky používania webovej stránky wevote4.eu potenciálnymi návštevníkmi alebo príjemcami.',
+    'subtitle_8' => '8. Zmeny všeobecných podmienok používania webových stránok',
+    'subtitle_2' => '2. Osobné údaje',
+    'line_7_1' => 'Ochrana informácií počas spracovania vašich osobných údajov je hlavným záujmom spoločnosti Commit Global, preto všetky osobné údaje zhromaždené počas návštev tejto stránky sú spracúvané v súlade s právnymi predpismi platnými v Rumunsku a Holandsku. Commit Global využíva najmodernejšie technológie a prijíma všetky primerané technické opatrenia na prenos a uchovávanie osobných údajov za podmienok úplnej bezpečnosti a dôvernosti.',
+    'line_3' => 'Commit Global spracúva všetky informácie opísané vyššie v odseku 2 „Osobné údaje“, ktoré sa používajú na ďalšie zlepšenie navigácie na našich webových stránkach a na pochopenie preferencií našich návštevníkov. Prostredníctvom tejto webovej stránky nebudeme zhromažďovať žiadne osobné údaje bez vášho výslovného súhlasu.',
+    'subtitle_6' => '6. Vaše práva',
+    'line_2' => 'Po návšteve wevote4.eu budeme automaticky zhromažďovať a ukladať údaje o vašej aktuálnej návšteve. Tieto informácie nie sú osobne identifikovateľné. Informácie, ktoré môžeme zhromažďovať, sú: operačný systém vo vašom zariadení, prehliadač, ktorý používate, stránky na wevote4.eu, ktoré ste navštívili, zdroj vašej návštevy (napr. Google, Facebook), dátum a čas vašej návštevy a doména, z ktorej pristupujete na internet.',
+    'line_4' => 'Poskytnuté osobné údaje sú dočasne uložené počas prevádzky platformy wevote4.eu.',
+    'title' => 'Zmluvné podmienky',
+    'line_6_3' => 'Žiadosť o úpravu alebo vymazanie vašich osobných údajov môžete kedykoľvek podať zaslaním jednoduchej žiadosti e-mailom na adrese: info@commitglobal.org',
+    'line_6_1' => 'Ako používateľ webovej stránky wevote4.eu máte nasledujúce práva:',
+    'subtitle_7' => '7. Označka a autorské práva',
+    'line_5' => 'Commit Global nebude poskytovať osobné údaje tretím stranám.',
+    'line_8_3' => 'Je prísne zakázané používať túto stránku za účelom zničenia alebo zmeny jej obsahu alebo bezpečnosti alebo na diskreditáciu alebo obťažovanie wevote4.eu alebo organizácie Commit Global a jej členov.',
+    'line_7_2' => 'Všetky osobné údaje na stránke sú uložené a spracúvané na serveroch nachádzajúcich sa v Európskej únii, ktoré spadajú pod európske právne predpisy o ochrane osobných údajov. Žiadne informácie uvedené na tejto stránke opúšťajú územie Európskej únie.',
+    'line_8_2' => 'Všetky práva sú vyhradené.',
+    'line_8_6' => 'Commit Global si vyhradzuje právo kedykoľvek revidovať a aktualizovať Všeobecné podmienky používania bez predchádzajúceho upozornenia alebo predchádzajúceho súhlasu používateľov.',
+    'line_7_4' => 'Ak zistíme incident týkajúci sa bezpečnosti osobných údajov, ktorý predstavuje riziko pre práva a slobody našich používateľov, informujeme Národný dozorný orgán pre spracovanie osobných údajov do 72 hodín. Ak bezpečnostný incident pravdepodobne predstavuje vysoké riziko pre vaše práva a slobody, budete tiež informovaní.',
+    'subtitle_5' => '5. Prenos osobných údajov tretím stranám',
+    'line_8_1' => 'Autorské práva k všetkým informáciám na tejto webovej stránke vlastní Commit Global a partneri projektu. Žiadny materiál na tejto stránke nemôže byť čiastočne, úplne reprodukovaný alebo upravený bez výslovného súhlasu vyjadreného spoločnosťou Commit Global, držiteľom tohto práva.',
+    'subtitle_3' => '3. Účel a dôvody',
+    'line_8_8' => 'Táto verzia obchodných podmienok wevote4.eu nadobúda účinnosť 23. mája 2024.',
+    'line_8_5' => '„Všeobecné podmienky používania\“ predstavujú v celom rozsahu dohodu uzatvorenú medzi vami a Commit Global týkajúcu sa používania tejto stránky.',
+    'line_7_3' => 'Commit Global sa zaväzuje, že nebude zverejňovať osobné údaje týkajúce sa vašich návštev wevote4.eu, s výnimkou právnych situácií alebo situácií uvedených v tomto dokumente Všeobecných podmienok.',
+    'line_6_2' => '- právo na informácie týkajúce sa spracovania osobných údajov spoločnosťou Commit Global;
+- právo na prístup k osobným údajom na požiadanie a bezplatne na jednu žiadosť ročne;
+- právo na zásah, opravu, vymazanie alebo prenosnosť osobných údajov na požiadanie a bezplatne;
+- Právo obrátiť sa na akýkoľvek príslušný orgán v súvislosti s otázkami ochrany osobných údajov v súvislosti s používaním vašich osobných údajov
+- právo na námietku a automatizovaný individuálny rozhodovací proces, ako aj právo obmedziť spracúvanie.',
+    'subtitle_1' => '1. Cieľ platformy',
+    'line_1' => 'Cieľom wevote4.eu je poskytovať informácie o procese hlasovania pre európske voľby.',
+    'line_8_4' => 'Commit Global použije všetky technické a organizačné bezpečnostné opatrenia na ochranu údajov, nad ktorými má kontrolu, pred akoukoľvek situáciou náhodnej alebo úmyselnej manipulácie, straty, zničenia alebo pred prístupom neoprávnenej osoby.',
+];
