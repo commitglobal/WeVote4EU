6cfae3b76203ef952fc66145d47513d327f5330f
Amazon Translate <noreply-mt-aws@weblate.org>
2024-05-23 13:07:31 +0000

    Translated using Weblate (Romanian)
    Currently translated at 100.0% (30 of 30 strings)
    
    Translation: WeVote4Eu/terms
    Translate-URL: https://translate.commitglobal.org/projects/wevote4eu/terms/ro/

--- lang/ro/terms.php
+++ lang/ro/terms.php
@@ -1,1 +1,37 @@
 <?php
+return [
+    'line_2' => 'Când vizitați wevote4.eu, vom colecta și stoca automat date despre vizita dvs. curentă. Aceste informații nu sunt identificabile personal. Informațiile pe care le putem colecta sunt: sistemul de operare de pe dispozitivul dvs., browserul pe care îl utilizați, paginile din wevote4.eu pe care le-ați vizitat, sursa vizitei dvs. (ex: Google, Facebook), data și ora vizitei dvs. și domeniul din care accesați internetul.',
+    'line_4' => 'Datele personale furnizate sunt stocate temporar în timpul funcționării platformei wevote4.eu.',
+    'title' => 'Termeni si Conditii',
+    'line_6_3' => 'Puteți trimite o solicitare de modificare sau ștergere a datelor dvs. personale în orice moment, trimițând o solicitare simplă prin e-mail la: info@commitglobal.org',
+    'line_7_2' => 'Toate datele personale de pe site sunt stocate și prelucrate pe servere situate în Uniunea Europeană, care intră sub incidența legislației europene privind protecția datelor cu caracter personal. Nicio informație furnizată pe acest site nu părăsește teritoriul Uniunii Europene.',
+    'line_8_2' => 'Toate drepturile sunt rezervate.',
+    'line_8_6' => 'Commit Global își rezervă dreptul de a revizui și actualiza „Termenii și condițiile generale de utilizare” în orice moment, fără notificare sau acceptare prealabilă a utilizatorilor.',
+    'line_7_4' => 'Dacă descoperim un incident privind securitatea datelor cu caracter personal care prezintă un risc pentru drepturile și libertățile utilizatorilor noștri, vom notifica Autoritatea Națională de Supraveghere a Prelucrării Datelor cu Caracter Personal în termen de 72 de ore. Dacă incidentul de securitate poate prezenta un risc ridicat pentru drepturile și libertățile dvs., veți fi, de asemenea, notificat.',
+    'subtitle_5' => '5. Transferul datelor cu caracter personal către terți',
+    'line_8_1' => 'Drepturile de autor pentru toate informațiile de pe acest site web sunt deținute de Commit Global și partenerii proiectului. Niciun material de pe acest site nu poate fi reprodus parțial, integral sau modificat fără consimțământul expres exprimat de Commit Global, deținătorul acestui drept.',
+    'subtitle_3' => '3. Scop și motive',
+    'line_8_8' => 'Această versiune a termenilor și condițiilor wevote4.eu intră în vigoare începând cu 23 mai 2024.',
+    'line_8_5' => '„Termenii și condițiile generale de utilizare” constituie, în întregime, un acord încheiat între dvs. și Commit Global cu privire la utilizarea acestui site.',
+    'line_7_3' => 'Commit Global se angajează să nu dezvăluie date cu caracter personal referitoare la vizitele dvs. pe wevote4.eu, cu excepția situațiilor legale sau a situațiilor prezentate în prezentul document Termeni și condiții.',
+    'line_6_2' => '- Dreptul la informare cu privire la prelucrarea datelor cu caracter personal de către Commit Global;
+- Dreptul de acces la datele cu caracter personal, la cerere și gratuit pentru o singură solicitare pe an;
+- Dreptul de intervenție, rectificare, ștergere sau portabilitate a datelor cu caracter personal, la cerere și gratuit;
+- Dreptul de a vă adresa oricărei autorități competente cu privire la problemele de confidențialitate a datelor în legătură cu utilizarea datelor dvs. cu caracter personal
+- Dreptul la opoziție și procesul automat de luare a deciziilor individuale, precum și dreptul de a restricționa prelucrarea.',
+    'subtitle_1' => '1. Obiectivul platformei',
+    'line_1' => 'Obiectivul wevote4.eu este de a oferi informații cu privire la procesul de vot pentru alegerile europene.',
+    'subtitle_7' => '7. Marcă comercială și drepturi de autor',
+    'line_8_4' => 'Commit Global va aplica toate măsurile tehnice și organizaționale de securitate pentru a proteja datele asupra cărora deține controlul împotriva oricărei situații de manipulare accidentală sau intenționată, pierdere, distrugere sau împotriva accesului unei persoane neautorizate.',
+    'line_6_1' => 'În calitate de utilizator al site-ului wevote4.eu aveți următoarele drepturi:',
+    'line_5' => 'Commit Global nu va furniza date cu caracter personal terților.',
+    'line_8_3' => 'Este strict interzisă utilizarea acestui Site în scopul distrugerii sau modificării acestuia, conținutului sau securității sale sau pentru discreditarea sau hărțuirea wevote4.eu sau a organizației Commit Global și a membrilor săi.',
+    'subtitle_4' => '4. Durata stocării datelor',
+    'line_8_7' => 'Dacă aveți întrebări cu privire la informațiile conținute în această pagină, vă rugăm să ne scrieți la info@commitglobal.org.',
+    'intro' => 'Termenii și condițiile generale de utilizare stabilesc termenii și condițiile generale de utilizare a site-ului wevote4.eu de către potențialii vizitatori sau beneficiari.',
+    'subtitle_8' => '8. Modificări ale Termenilor și Condițiilor Generale de Utilizare a Site-ului',
+    'subtitle_2' => '2. Date cu caracter personal',
+    'line_7_1' => 'Protecția informațiilor în timpul prelucrării datelor dvs. personale este o preocupare majoră a Commit Global, prin urmare toate datele personale colectate în timpul vizitelor pe acest site sunt prelucrate în conformitate cu prevederile legale în vigoare în România și Olanda. Commit Global utilizează tehnologii de ultimă generație și ia toate măsurile tehnice rezonabile pentru transmiterea și stocarea datelor cu caracter personal în condiții de securitate și confidențialitate deplină.',
+    'line_3' => 'Commit Global procesează toate informațiile descrise mai sus, în paragraful 2 „Date personale”, care sunt utilizate pentru a îmbunătăți în continuare experiența de navigare pe site-ul nostru web și pentru a înțelege preferințele vizitatorilor noștri. Nu vom colecta informații personale prin intermediul acestui site fără consimțământul dvs. expres.',
+    'subtitle_6' => '6. Drepturile tale',
+];
