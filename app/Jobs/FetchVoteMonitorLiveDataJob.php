84a9d768d5a06c047ce1437d700630a19a187d50
Andrei Ioniță <hi@andrei.io>
2024-06-06 12:10:18 +0100

    feat: add votemonitor integration

---
+++ app/Jobs/FetchVoteMonitorLiveDataJob.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Jobs;
+
+use App\Models\Stat;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Http;
+
+class FetchVoteMonitorLiveDataJob implements ShouldQueue, ShouldBeUnique
+{
+    use Dispatchable;
+    use InteractsWithQueue;
+    use Queueable;
+    use SerializesModels;
+
+    /**
+     * The number of times the job may be attempted.
+     *
+     * @var int
+     */
+    public $tries = 1;
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        $data = Http::acceptJson()
+            ->withToken(config('services.votemonitor.apikey'))
+            ->get(config('services.votemonitor.url'))
+            ->throw()
+            ->json();
+
+        Stat::upsert(
+            collect($data)
+                ->map(fn ($value, $key) => [
+                    'key' => $key,
+                    'value' => $value,
+                    'updated_at' => now(),
+                ])
+                ->values()
+                ->all(),
+            uniqueBy: ['key'],
+            update: ['value', 'updated_at'],
+        );
+    }
+}
--- config/services.php
+++ config/services.php
@@ -39,4 +39,11 @@ return [
 
     'google_analytics_id' => env('GOOGLE_ANALYTICS_ID'),
 
+    'votemonitor' => [
+        'enabled' => env('VOTEMONITOR_ENABLED', false),
+        'url' => env('VOTEMONITOR_URL'),
+        'apikey' => env('VOTEMONITOR_APIKEY'),
+        'election_round_id' => env('VOTEMONITOR_ELECTION_ROUND_ID'),
+    ],
+
 ];
