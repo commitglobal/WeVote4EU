5bcfc8ec4d62e44fbc2c2fa59fea21bafa74d03e
Weblate <noreply@weblate.org>
2025-10-18 18:05:20 +0000

    Translated using Weblate (Irish)
    Currently translated at 100.0% (30 of 30 strings)
    
    Translated using Weblate (Irish)
    
    Currently translated at 100.0% (30 of 30 strings)
    
    Translated using Weblate (Irish)
    
    Currently translated at 100.0% (5 of 5 strings)
    
    Translated using Weblate (Irish)
    
    Currently translated at 100.0% (33 of 33 strings)
    
    Translated using Weblate (Irish)
    
    Currently translated at 100.0% (23 of 23 strings)
    
    Translated using Weblate (Irish)
    
    Currently translated at 100.0% (36 of 36 strings)
    
    Co-authored-by: Amazon Translate <noreply-mt-aws@weblate.org>
    Co-authored-by: Weblate <noreply-mt-weblate@weblate.org>
    Co-authored-by: Weblate <noreply@weblate.org>
    Translate-URL: https://translate.commitglobal.org/projects/wevote4eu/about/ga/
    Translate-URL: https://translate.commitglobal.org/projects/wevote4eu/app/ga/
    Translate-URL: https://translate.commitglobal.org/projects/wevote4eu/partners/ga/
    Translate-URL: https://translate.commitglobal.org/projects/wevote4eu/privacy/ga/
    Translate-URL: https://translate.commitglobal.org/projects/wevote4eu/terms/ga/
    Translation: WeVote4Eu/about
    Translation: WeVote4Eu/app
    Translation: WeVote4Eu/partners
    Translation: WeVote4Eu/privacy
    Translation: WeVote4Eu/terms

--- lang/ga/partners.php
+++ lang/ga/partners.php
@@ -1,4 +1,8 @@
 <?php
 return [
     'title' => 'Comhpháirtithe',
+    'intro_1' => 'Táimid ag iarraidh guth na sochaí sibhialta a mhéadú agus spás a sholáthar do neamhrialtasacha neamhrialtasacha ar fud an Aontais chun na toghcháin a chlúdach agus anailísiú óna d',
+    'intro_2' => 'Ní bheadh an rannán Live Feed ann ach coincheap, gan rannpháirtithe a bheith rannpháirtithe, a d\'fhreagair ár gcuireadh chun páirt a ghlacadh sa tionscadal seo agus a gcuid léargas agus tráchtaireachtaí a thairiscint i rith laethanta na dtoghcháin.',
+    'institutional' => 'Chomhpháirtithe',
+    'experts' => 'Saineolaithe neamhsple',
 ];
